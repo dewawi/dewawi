6148ae3b48013ff732aa75843a885dff802591a2
kseones <remzi.demen@deec.de>
2019-12-12 21:18:07 +0300

    add CSV support

--- application/forms/Upload.php
+++ application/forms/Upload.php
@@ -12,7 +12,7 @@ class Application_Form_Upload extends Zend_Form
 			->setRequired(true)
 			->addValidator('Count', false, 1)
 			->addValidator('Size', false, 10240000)
-			->addValidator('Extension', false, 'pdf,jpg,jpeg,png,gif');
+			->addValidator('Extension', false, 'pdf,jpg,jpeg,png,gif,csv');
 
 		$submit = new Zend_Form_Element_Submit('submit');
 		$submit->setLabel('UPLOAD_SUBMIT')
