23baeb5fa0277e20f20fa2472a56e08fc26cb53c
kseones <remzi@dewawi.com>
2022-11-11 12:31:45 +0100

    added media upload page to admin

---
+++ application/modules/admin/controllers/UploadController.php
@@ -0,0 +1,37 @@
+<?php
+
+class Admin_UploadController extends Zend_Controller_Action
+{
+	protected $_date = null;
+
+	protected $_user = null;
+
+	/**
+	 * FlashMessenger
+	 *
+	 * @var Zend_Controller_Action_Helper_FlashMessenger
+	 */
+	protected $_flashMessenger = null;
+
+	public function init()
+	{
+		$params = $this->_getAllParams();
+
+		$this->_date = date('Y-m-d H:i:s');
+
+		$this->view->id = isset($params['id']) ? $params['id'] : 0;
+		$this->view->action = $params['action'];
+		$this->view->controller = $params['controller'];
+		$this->view->module = $params['module'];
+		$this->view->user = $this->_user = Zend_Registry::get('User');
+		$this->view->mainmenu = $this->_helper->MainMenu->getMainMenu();
+
+		$this->_flashMessenger = $this->_helper->getHelper('FlashMessenger');
+	}
+
+	public function indexAction()
+	{
+		if($this->getRequest()->isPost()) $this->_helper->getHelper('layout')->disableLayout();
+		$this->view->messages = $this->_flashMessenger->getMessages();
+	}
+}
