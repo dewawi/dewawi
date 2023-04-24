dce7e9f324eefc1e5e5491e30dabe84570ae7a41
Damian Wessels <damianbodde@gmail.com>
2023-04-24 13:02:40 +0200

    Wrong parameter in Filemanager

--- library/FileManager/config/config.php
+++ library/FileManager/config/config.php
@@ -21,7 +21,7 @@ if(isset($_SESSION['RF']['writable']) && $_SESSION['RF']['writable']) $writable 
 
 mb_internal_encoding('UTF-8');
 mb_http_output('UTF-8');
-mb_http_input('UTF-8');
+mb_http_input('p');
 mb_language('uni');
 mb_regex_encoding('UTF-8');
 ob_start('mb_output_handler');
