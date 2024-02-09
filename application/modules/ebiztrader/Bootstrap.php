2a25dbacc8a8dc66fa9332a380ee70fd2b19c963
kseones <remzi@dewawi.com>
2024-02-09 20:37:51 +0100

    remove unused files

--- application/modules/ebiztrader/Bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Ebiztrader_Bootstrap extends Zend_Application_Module_Bootstrap
-{
-	protected function _initPlugins() {
-		$front = Zend_Controller_Front::getInstance();
-		$front->registerPlugin(new Ebiztrader_Plugin_Helper());
-	}
-}
