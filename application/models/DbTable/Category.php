88cab5631353524591c4f167399816df4c2b6c9a
kseones <remzi@dewawi.com>
2024-10-26 17:47:35 +0200

    optimized category fetching

--- application/models/DbTable/Category.php
+++ application/models/DbTable/Category.php
@@ -20,58 +20,46 @@ class Application_Model_DbTable_Category extends Zend_Db_Table_Abstract
 
 	public function getCategories($type, $parentid = null)
 	{
-		$where = array();
-		if($parentid !== null) {
+		// Prepare the where conditions
+		$where = [];
+		if ($parentid !== null) {
 			$where[] = $this->getAdapter()->quoteInto('parentid = ?', $parentid);
 		}
 		$where[] = $this->getAdapter()->quoteInto('type = ?', $type);
 		$where[] = $this->getAdapter()->quoteInto('clientid = ?', $this->_client['id']);
+		$where[] = $this->getAdapter()->quoteInto('activated = ?', 1);
 		$where[] = $this->getAdapter()->quoteInto('deleted = ?', 0);
+
+		// Fetch the data
 		$data = $this->fetchAll($where, 'ordering');
 
-		$categories = array();
-		foreach($data as $category) {
-			if(!$category->parentid) {
-				$categories[$category->id]['id'] = $category->id;
-				if(isset($category->shopid)) {
-					$categories[$category->id]['shopid'] = $category->shopid;
-					//$categories[$category->id]['shopcatid'] = $category->shopcatid;
-				}
-				$categories[$category->id]['type'] = $category->type;
-				$categories[$category->id]['title'] = $category->title;
-				$categories[$category->id]['image'] = $category->image;
-				$categories[$category->id]['description'] = $category->description;
-				$categories[$category->id]['footer'] = $category->footer;
-				$categories[$category->id]['parentid'] = $category->parentid;
-				$categories[$category->id]['ordering'] = $category->ordering;
-				if($category->parentid) {
-					if(!isset($categories[$category->parentid])) $categories[$category->parentid] = array();
-					if(!isset($categories[$category->parentid]['childs'])) $categories[$category->parentid]['childs'] = array();
-					array_push($categories[$category->parentid]['childs'], $category->id);
-				}
+		// Initialize categories array
+		$categories = [];
+
+		// Iterate through the data
+		foreach ($data as $category) {
+			// Prepare the category array
+			$categories[$category->id] = [
+				'id' => $category->id,
+				'type' => $category->type,
+				'title' => $category->title,
+				'subtitle' => $category->subtitle,
+				'image' => $category->image,
+				'description' => $category->description,
+				'footer' => $category->footer,
+				'parentid' => $category->parentid,
+				'ordering' => $category->ordering,
+				'shopid' => isset($category->shopid) ? $category->shopid : null,
+				//'shopcatid' => isset($category->shopcatid) ? $category->shopcatid : null
+			];
+		}
+		// If the category has a parent, add it to the parent's 'childs' array
+		foreach ($data as $category) {
+			if ($category->parentid && isset($categories[$category->parentid])) {
+				$categories[$category->parentid]['childs'][] = $category->id;
 			}
 		}
-		foreach($data as $category) {
-			if($category->parentid) {
-				$categories[$category->id]['id'] = $category->id;
-				if(isset($category->shopid)) {
-					$categories[$category->id]['shopid'] = $category->shopid;
-					//$categories[$category->id]['shopcatid'] = $category->shopcatid;
-				}
-				$categories[$category->id]['type'] = $category->type;
-				$categories[$category->id]['title'] = $category->title;
-				$categories[$category->id]['image'] = $category->image;
-				$categories[$category->id]['description'] = $category->description;
-				$categories[$category->id]['footer'] = $category->footer;
-				$categories[$category->id]['parentid'] = $category->parentid;
-				$categories[$category->id]['ordering'] = $category->ordering;
-				if($category->parentid) {
-					if(!isset($categories[$category->parentid])) $categories[$category->parentid] = array();
-					if(!isset($categories[$category->parentid]['childs'])) $categories[$category->parentid]['childs'] = array();
-					array_push($categories[$category->parentid]['childs'], $category->id);
-				}
-			}
-		}
+
 		return $categories;
 	}
 }
