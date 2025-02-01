ea6154d8f16f0a01f8a7da9ed1570595247d2432
kseones <remzi@dewawi.com>
2025-02-01 09:47:18 +0100

    added subject and reference to the sales documents

--- install/config/update-1.1.1-1.1.2.sql
+++ install/config/update-1.1.1-1.1.2.sql
@@ -65,3 +65,23 @@ CREATE TABLE IF NOT EXISTS `shoporderpos` (
 
 ALTER TABLE `page` ADD `parentid` int(11) NOT NULL DEFAULT 0 AFTER `id`;
 ALTER TABLE `page` ADD `type` varchar(255) DEFAULT NULL AFTER `shopid`;
+
+ALTER TABLE `creditnote` ADD `subject` varchar(255) DEFAULT NULL AFTER `title`;
+ALTER TABLE `deliveryorder` ADD `subject` varchar(255) DEFAULT NULL AFTER `title`;
+ALTER TABLE `invoice` ADD `subject` varchar(255) DEFAULT NULL AFTER `title`;
+ALTER TABLE `quote` ADD `subject` varchar(255) DEFAULT NULL AFTER `title`;
+ALTER TABLE `reminder` ADD `subject` varchar(255) DEFAULT NULL AFTER `title`;
+ALTER TABLE `salesorder` ADD `subject` varchar(255) DEFAULT NULL AFTER `title`;
+ALTER TABLE `quoterequest` ADD `subject` varchar(255) DEFAULT NULL AFTER `title`;
+ALTER TABLE `purchaseorder` ADD `subject` varchar(255) DEFAULT NULL AFTER `title`;
+ALTER TABLE `process` ADD `subject` varchar(255) DEFAULT NULL AFTER `title`;
+
+ALTER TABLE `creditnote` ADD `reference` varchar(255) DEFAULT NULL AFTER `subject`;
+ALTER TABLE `deliveryorder` ADD `reference` varchar(255) DEFAULT NULL AFTER `subject`;
+ALTER TABLE `invoice` ADD `reference` varchar(255) DEFAULT NULL AFTER `subject`;
+ALTER TABLE `quote` ADD `reference` varchar(255) DEFAULT NULL AFTER `subject`;
+ALTER TABLE `reminder` ADD `reference` varchar(255) DEFAULT NULL AFTER `subject`;
+ALTER TABLE `salesorder` ADD `reference` varchar(255) DEFAULT NULL AFTER `subject`;
+ALTER TABLE `quoterequest` ADD `reference` varchar(255) DEFAULT NULL AFTER `subject`;
+ALTER TABLE `purchaseorder` ADD `reference` varchar(255) DEFAULT NULL AFTER `subject`;
+ALTER TABLE `process` ADD `reference` varchar(255) DEFAULT NULL AFTER `subject`;
