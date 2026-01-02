0fae1871debc74c899456cff5ce805663de714a1
kseones <remzi@dewawi.com>
2026-01-02 15:27:29 +0100

    added warehouse model

---
+++ application/models/DbTable/Warehouse.php
@@ -0,0 +1,44 @@
+<?php
+
+class Application_Model_DbTable_Warehouse extends Zend_Db_Table_Abstract
+{
+
+	protected $_name = 'warehouse';
+
+	protected $_date = null;
+
+	protected $_user = null;
+
+	protected $_client = null;
+
+	public function init()
+	{
+		$this->_date = date('Y-m-d H:i:s');
+		$this->_user = Zend_Registry::get('User');
+		$this->_client = Zend_Registry::get('Client');
+	}
+
+	public function getWarehouse($id)
+	{
+		$id = (int)$id;
+		$row = $this->fetchRow('id = ' . $id);
+		if (!$row) {
+			throw new Exception("Could not find row $id");
+		}
+		return $row->toArray();
+	}
+
+	public function getWarehouses()
+	{
+		$where = array();
+		$where[] = $this->getAdapter()->quoteInto('clientid = ?', $this->_client['id']);
+		$where[] = $this->getAdapter()->quoteInto('deleted = ?', 0);
+		$data = $this->fetchAll($where);
+
+		$uoms = array();
+		foreach($data as $uom) {
+			$uoms[$uom->id] = $uom->title;
+		}
+		return $uoms;
+	}
+}
