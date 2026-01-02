b2778197471d81c3394df90bd593c0dae3f1f8ff
kseones <remzi@dewawi.com>
2026-01-02 12:48:11 +0100

    update ledger controller to manage entries with item id

--- application/modules/items/controllers/LedgerController.php
+++ application/modules/items/controllers/LedgerController.php
@@ -77,7 +77,7 @@ class Items_LedgerController extends Zend_Controller_Action
 		$get = new Items_Model_Get();
 		$ledger = $get->ledger($params, $options);
 
-		$this->view->items = $items;
+		$this->view->ledger = $ledger;
 		$this->view->options = $options;
 		$this->view->toolbar = $toolbar;
 		$this->view->messages = $this->_flashMessenger->getMessages();
@@ -87,6 +87,7 @@ class Items_LedgerController extends Zend_Controller_Action
 	{
 		$request = $this->getRequest();
 		$locale = Zend_Registry::get('Zend_Locale');
+		$itemid = $this->_getParam('itemid', 0);
 		$catid = $this->_getParam('catid', 0);
 
 		$form = new Items_Form_Ledger();
@@ -95,45 +96,68 @@ class Items_LedgerController extends Zend_Controller_Action
 
 		if($request->isPost()) {
 			$data = $request->getPost();
-			if($form->isValid($data)) {
-		        // normalize quantity
-		        if (isset($data['quantity'])) {
-		            $data['quantity'] = Zend_Locale_Format::getNumber(
-		                $data['quantity'],
-		                array('precision' => 4, 'locale' => $locale)
-		            );
-		        }
-
-				$data['docid'] = 0;
-				$data['doctype'] = '';
-				$data['comment'] = '';
-				$data['language'] = '';
-				$data['warehouseid'] = 0;
-
-	            $itemDb = new Items_Model_DbTable_Item();
-				$ledgerDb = new Items_Model_DbTable_Ledger();
-				$ledgerDb->addLedger($data);
-
+			if($form->isValidPartial($data)) {
+				$itemDb = new Items_Model_DbTable_Item();
 				if($item = $itemDb->getItemBySKU($data['sku'])) {
-		            // calculate delta and new quantity
-		            $delta = (float)$data['quantity'];
-		            if ($type === 'outflow') {
-		                $delta = -$delta;
-		            }
-		            $newQty = ((float)$item['quantity']) + $delta;
+					// normalize quantity
+					if (isset($data['quantity'])) {
+						$data['quantity'] = Zend_Locale_Format::getNumber(
+							$data['quantity'],
+							array('precision' => 4, 'locale' => $locale)
+						);
+					}
+
+					$data['itemid'] = $item['id'];
+					$data['docid'] = 0;
+					$data['doctype'] = '';
+					$data['language'] = '';
+
+					$ledgerdate = new Zend_Date($data['ledgerdate'], Zend_Date::DATES, 'de');
+					$data['ledgerdate'] = $ledgerdate->get('yyyy-MM-dd');
+
+					$ledgerDb = new Items_Model_DbTable_Ledger();
+					$ledgerDb->addLedger($data);
+
+					// calculate delta and new quantity
+					$delta = (float)$data['quantity'];
+					if ($data['type'] === 'outflow') {
+						$delta = -$delta;
+					}
+					$newQty = ((float)$item['quantity']) + $delta;
 
 					$itemDb->updateItem($item['id'], array(
-                    	'quantity' => $newQty
-                	));
-				}
+						'quantity' => $newQty
+					));
 
-				$this->_helper->redirector('index');
+					$this->_helper->redirector('index');
+				} else {
+					$message = sprintf(
+						$this->view->translate('MESSAGES_NO_ITEM_FOUND'),
+						$data['sku']
+					);
+
+					$this->_flashMessenger->addMessage($message);
+				}
 			} else {
 				$form->populate($data);
 			}
+		} else {
+			$ledgerdate = date('d.m.Y');
+			$data = array();
+			$data['comment'] = 'Booking '.$ledgerdate;
+			$data['ledgerdate'] = $ledgerdate;
+			if($itemid > 0) {
+				$itemDb = new Items_Model_DbTable_Item();
+				if($item = $itemDb->getItem($itemid)) {
+					$data['sku'] = $item['sku'];
+					$data['comment'] = $item['title'];
+				}
+			}
+			$form->populate($data);
 		}
 		$this->view->form = $form;
 		$this->view->toolbar = $toolbar;
+		$this->view->messages = $this->_flashMessenger->getMessages();
 	}
 
 	public function editAction()
@@ -159,65 +183,56 @@ class Items_LedgerController extends Zend_Controller_Action
 				$data = $request->getPost();
 				$element = key($data);
 				if(isset($form->$element) && $form->isValidPartial($data)) {
-					if(isset($data['ledgerdate'])) {
-						if(Zend_Date::isDate($data['ledgerdate'])) {
-							$ledgerdate = new Zend_Date($data['ledgerdate'], Zend_Date::DATES, 'de');
-							$data['ledgerdate'] = $ledgerdate->get('yyyy-MM-dd');
-						} else {
-							$data['ledgerdate'] = NULL;
-						}
-					}
-					if(array_key_exists('quantity', $data)) {
-						$locale = Zend_Registry::get('Zend_Locale');
-						$data['quantity'] = Zend_Locale_Format::getNumber($data['quantity'],array('precision' => 2,'locale' => $locale));
-					}
 
-        			$itemDb = new Items_Model_DbTable_Item();
+					// normalize (trim + parse)
+					$data = DEEC_Filter::normalizeByFormat($data, $element);
+
+					$itemDb = new Items_Model_DbTable_Item();
 					$oldLedger = $ledger;
 
-		            // update ledger
+					// update ledger
 					$ledgerDb->updateLedger($id, $data);
 					$newLedger = $ledgerDb->getLedger($id);
 
-				    $oldSku = $oldLedger['sku'];
-				    $newSku = $newLedger['sku'];
+					$oldSku = $oldLedger['sku'];
+					$newSku = $newLedger['sku'];
 
-				    // signed quantities: inflow = +qty, outflow = -qty
-				    $oldSigned = (float)$oldLedger['quantity'];
-				    if($oldLedger['type'] === 'outflow') $oldSigned = -$oldSigned;
+					// signed quantities: inflow = +qty, outflow = -qty
+					$oldSigned = (float)$oldLedger['quantity'];
+					if($oldLedger['type'] === 'outflow') $oldSigned = -$oldSigned;
 
-				    $newSigned = (float)$newLedger['quantity'];
-				    if($newLedger['type'] === 'outflow') $newSigned = -$newSigned;
+					$newSigned = (float)$newLedger['quantity'];
+					if($newLedger['type'] === 'outflow') $newSigned = -$newSigned;
 
 
 					if($oldSku !== $newSku) {
-                		// SKU changed: revert old effect on old item, apply new effect on new item
+						// SKU changed: revert old effect on old item, apply new effect on new item
 						$oldItem = $itemDb->getItemBySKU($oldSku);
 						$newItem = $itemDb->getItemBySKU($newSku);
 
 						if($oldItem) {
-						    // remove old effect from old item
-						    $oldItemQty = ((float)$oldItem['quantity']) - $oldSigned;
+							// remove old effect from old item
+							$oldItemQty = ((float)$oldItem['quantity']) - $oldSigned;
 
-						    $itemDb->updateItem($oldItem['id'], array('quantity' => $oldItemQty));
+							$itemDb->updateItem($oldItem['id'], array('quantity' => $oldItemQty));
 						}
 
 						if($newItem) {
-						    // apply new effect to new item
-						    $newItemQty = ((float)$newItem['quantity']) + $newSigned;
-						    $itemDb->updateItem($newItem['id'], array('quantity' => $newItemQty));
+							// apply new effect to new item
+							$newItemQty = ((float)$newItem['quantity']) + $newSigned;
+							$itemDb->updateItem($newItem['id'], array('quantity' => $newItemQty));
 						}
 					} else {
-                		// same SKU: apply only the difference
-				        $delta = $newSigned - $oldSigned;
+						// same SKU: apply only the difference
+						$delta = $newSigned - $oldSigned;
 
-				        if($delta != 0.0) {
-				            if($item = $itemDb->getItemBySKU($oldSku)) {
-						        $qty = ((float)$item['quantity']) + $delta;
+						if($delta != 0.0) {
+							if($item = $itemDb->getItemBySKU($oldSku)) {
+								$qty = ((float)$item['quantity']) + $delta;
 
-						        $itemDb->updateItem($item['id'], array('quantity' => $qty));
-				            }
-				        }
+								$itemDb->updateItem($item['id'], array('quantity' => $qty));
+							}
+						}
 					}
 
 					echo Zend_Json::encode($ledgerDb->getLedger($id));
@@ -239,7 +254,7 @@ class Items_LedgerController extends Zend_Controller_Action
 							$data['quantity'],
 							array(
 								'precision' => 2,
-								'locale'    => $locale
+								'locale' => $locale
 							)
 						);
 					}
@@ -269,32 +284,30 @@ class Items_LedgerController extends Zend_Controller_Action
 		$this->_helper->getHelper('layout')->disableLayout();
 
 		$id = $this->_getParam('id', 0);
-		$item = new Items_Model_DbTable_Item();
-		$data = $item->getItem($id);
+		$ledger = new Items_Model_DbTable_Ledger();
+		$data = $ledger->getLedger($id);
 		unset($data['id']);
-		$data['quantity'] = 0;
-		$data['inventory'] = 1;
-		$data['title'] = $data['title'].' 2';
 		$data['modified'] = NULL;
 		$data['modifiedby'] = 0;
 		$data['locked'] = 0;
 		$data['lockedtime'] = NULL;
-		echo $itemid = $item->addItem($data);
+		echo $itemid = $ledger->addLedger($data);
+
+		// Update item quantity
+		$itemDb = new Items_Model_DbTable_Item();
+		if($item = $itemDb->getItem($data['itemid'])) {
+			// compute signed qty (inflow +, outflow -)
+			$signed = (float)$data['quantity'];
+			if($data['type'] === 'outflow') $signed = -$signed;
+
+			// reverse effect
+			$delta = $signed;
+
+			$newQty = ((float)$item['quantity']) + $delta;
+			$itemDb->updateItem($item['id'], array('quantity' => $newQty));
+		}
 
 		$this->_flashMessenger->addMessage('MESSAGES_SUCCESFULLY_COPIED');
-	}
-
-	public function deleteAction()
-	{
-		$this->_helper->viewRenderer->setNoRender();
-		$this->_helper->getHelper('layout')->disableLayout();
-
-		if ($this->getRequest()->isPost()) {
-			$id = $this->_getParam('id', 0);
-			$item = new Items_Model_DbTable_Item();
-			$item->deleteItem($id);
-		}
-		$this->_flashMessenger->addMessage('MESSAGES_SUCCESFULLY_DELETED');
 	}
 
 	public function lockAction()
