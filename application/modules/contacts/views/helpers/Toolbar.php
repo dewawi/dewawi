097557bdca5d5280eb06edce71430cb35b00e6d6
kseones <remzi@dewawi.com>
2026-03-28 15:20:12 +0100

    updated toolbar helpers with the new forms

--- application/modules/contacts/views/helpers/Toolbar.php
+++ application/modules/contacts/views/helpers/Toolbar.php
@@ -4,7 +4,60 @@
 */
 class Zend_View_Helper_Toolbar extends Zend_View_Helper_Abstract
 {
-	public function Toolbar() { ?>
+	public function Toolbar(): string
+	{
+		$v = $this->view;
+		$tb = $v->toolbar;
+
+		if (!$tb || !method_exists($tb, 'renderElement')) {
+			return '';
+		}
+
+		$html = '';
+
+		if ($v->action === 'edit') {
+			$html .= '<input class="id" type="hidden" value="'.htmlspecialchars((string)$v->id).'" name="id"/>';
+			$html .= $tb->renderElement('copy');
+			$html .= $tb->renderElement('delete');
+			return $html;
+		}
+
+		if ($v->action === 'add') {
+			$html .= $tb->renderElement('save');
+			return $html;
+		}
+
+		if ($v->action === 'select') {
+			$html .= '<span>'.htmlspecialchars($v->translate('TOOLBAR_SEARCH')).'</span>';
+			$html .= $tb->renderElement('keyword');
+			$html .= $tb->renderElement('clear');
+			$html .= $tb->renderElement('limit');
+			$html .= $tb->renderElement('catid');
+			$html .= '<input id="type" type="hidden" name="type" value="select"/>';
+			return $html;
+		}
+
+		if ($v->action === 'index') {
+			$html .= $tb->renderElement('add');
+			$html .= $tb->renderElement('edit');
+			$html .= $tb->renderElement('copy');
+			$html .= $tb->renderElement('delete');
+			$html .= $tb->renderElement('keyword');
+			$html .= $tb->renderElement('clear');
+			$html .= $tb->renderElement('reset');
+			$html .= $tb->renderElement('order');
+			$html .= $tb->renderElement('sort');
+			$html .= $tb->renderElement('country');
+			$html .= $tb->renderElement('limit');
+			$html .= $tb->renderElement('catid');
+			$html .= $tb->renderElement('tagid');
+			return $html;
+		}
+
+		return $html;
+	}
+}
+/*	public function Toolbar() { ?>
 		<?php if($this->view->controller == 'contact') : ?>
 			<?php if($this->view->action == 'edit') : ?>
 				<input class="id" type="hidden" value="<?php echo $this->view->id ?>" name="id"/>
@@ -47,4 +100,4 @@ class Zend_View_Helper_Toolbar extends Zend_View_Helper_Abstract
 			<?php endif; ?>
 		<?php endif;
 	}
-}
+}*/
--- application/modules/items/views/helpers/Toolbar.php
+++ application/modules/items/views/helpers/Toolbar.php
@@ -1,59 +1,87 @@
 <?php
 /**
 * Class inserts neccery code for Toolbar
-*/
-class Zend_View_Helper_Toolbar extends Zend_View_Helper_Abstract
+*/class Zend_View_Helper_Toolbar extends Zend_View_Helper_Abstract
 {
-	public function Toolbar() { ?>
-		<?php if($this->view->action == 'edit') : ?>
-			<input class="id" type="hidden" value="<?php echo $this->view->id ?>" name="id"/>
-			<?php echo $this->view->toolbar->copy; ?>
-			<?php echo $this->view->toolbar->delete; ?>
-		<?php elseif($this->view->action == 'add') : ?>
-			<?php echo $this->view->toolbar->save; ?>
-		<?php elseif($this->view->action == 'select') : ?>
-			<span><?php echo $this->view->translate('TOOLBAR_SEARCH') ?></span>
-			<?php echo $this->view->toolbar->keyword; ?>
-			<?php echo $this->view->toolbar->clear; ?>
-			<?php echo $this->view->toolbar->limit; ?>
-			<?php echo $this->view->toolbar->catid; ?>
-			<input id="type" type="hidden" name="type" value="select"/>
-		<?php elseif($this->view->action == 'index') : ?>
-			<?php if($this->view->controller == 'item') : ?>
-				<?php echo $this->view->toolbar->add; ?>
-				<?php echo $this->view->toolbar->edit; ?>
-				<?php echo $this->view->toolbar->copy; ?>
-				<?php echo $this->view->toolbar->delete; ?>
-				<?php echo $this->view->toolbar->keyword; ?>
-				<?php echo $this->view->toolbar->clear; ?>
-				<?php echo $this->view->toolbar->reset; ?>
-				<?php echo $this->view->toolbar->order; ?>
-				<?php echo $this->view->toolbar->sort; ?>
-				<?php //echo $this->view->toolbar->manufacturerid; ?>
-				<?php echo $this->view->toolbar->limit; ?>
-				<?php echo $this->view->toolbar->catid; ?>
-				<?php echo $this->view->toolbar->tagid; ?>
-			<?php elseif($this->view->controller == 'pricerule') : ?>
-				<?php echo $this->view->toolbar->add; ?>
-				<?php echo $this->view->toolbar->edit; ?>
-				<?php echo $this->view->toolbar->copy; ?>
-				<?php echo $this->view->toolbar->delete; ?>
-				<?php echo $this->view->toolbar->keyword; ?>
-				<?php echo $this->view->toolbar->clear; ?>
-				<?php echo $this->view->toolbar->reset; ?>
-				<?php echo $this->view->toolbar->limit; ?>
-			<?php else : ?>
-				<?php echo $this->view->toolbar->add; ?>
-				<?php echo $this->view->toolbar->edit; ?>
-				<?php echo $this->view->toolbar->copy; ?>
-				<?php echo $this->view->toolbar->delete; ?>
-				<?php echo $this->view->toolbar->keyword; ?>
-				<?php echo $this->view->toolbar->clear; ?>
-				<?php echo $this->view->toolbar->reset; ?>
-				<?php echo $this->view->toolbar->order; ?>
-				<?php echo $this->view->toolbar->sort; ?>
-				<?php echo $this->view->toolbar->limit; ?>
-			<?php endif; ?>
-		<?php endif;
+	public function Toolbar(): string
+	{
+		$v = $this->view;
+		$tb = $v->toolbar;
+
+		if (!$tb || !method_exists($tb, 'renderElement')) {
+			return '';
+		}
+
+		$html = '';
+
+		if ($v->action === 'edit') {
+			$html .= '<input class="id" type="hidden" value="'.htmlspecialchars((string)$v->id).'" name="id"/>';
+			$html .= $tb->renderElement('copy');
+			$html .= $tb->renderElement('delete');
+			return $html;
+		}
+
+		if ($v->action === 'add') {
+			$html .= $tb->renderElement('save');
+			return $html;
+		}
+
+		if ($v->action === 'select') {
+			$html .= '<span>'.htmlspecialchars($v->translate('TOOLBAR_SEARCH')).'</span>';
+			$html .= $tb->renderElement('keyword');
+			$html .= $tb->renderElement('clear');
+			$html .= $tb->renderElement('limit');
+			$html .= $tb->renderElement('catid');
+			$html .= '<input id="type" type="hidden" name="type" value="select"/>';
+			return $html;
+		}
+
+		if ($v->action === 'index') {
+
+			// item: currently commented out in legacy
+			if ($v->controller === 'item') {
+				$html .= $tb->renderElement('add');
+				$html .= $tb->renderElement('edit');
+				$html .= $tb->renderElement('copy');
+				$html .= $tb->renderElement('delete');
+				$html .= $tb->renderElement('keyword');
+				$html .= $tb->renderElement('clear');
+				$html .= $tb->renderElement('reset');
+				$html .= $tb->renderElement('order');
+				$html .= $tb->renderElement('sort');
+				$html .= $tb->renderElement('manufacturerid');
+				$html .= $tb->renderElement('limit');
+				$html .= $tb->renderElement('catid');
+				$html .= $tb->renderElement('tagid');
+				return $html;
+			}
+
+			if ($v->controller === 'pricerule') {
+				$html .= $tb->renderElement('add');
+				$html .= $tb->renderElement('edit');
+				$html .= $tb->renderElement('copy');
+				$html .= $tb->renderElement('delete');
+				$html .= $tb->renderElement('keyword');
+				$html .= $tb->renderElement('clear');
+				$html .= $tb->renderElement('reset');
+				$html .= $tb->renderElement('limit');
+				return $html;
+			}
+
+			// default
+			$html .= $tb->renderElement('add');
+			$html .= $tb->renderElement('edit');
+			$html .= $tb->renderElement('copy');
+			$html .= $tb->renderElement('delete');
+			$html .= $tb->renderElement('keyword');
+			$html .= $tb->renderElement('clear');
+			$html .= $tb->renderElement('reset');
+			$html .= $tb->renderElement('order');
+			$html .= $tb->renderElement('sort');
+			$html .= $tb->renderElement('limit');
+			return $html;
+		}
+
+		return $html;
 	}
 }
