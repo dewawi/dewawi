bed31789b6d7d0dc6651024c6b2ba70d69a20efc
kseones <remzi@dewawi.com>
2026-03-28 18:07:41 +0100

    updated pagination helper to work with the new forms

--- application/views/helpers/Pagination.php
+++ application/views/helpers/Pagination.php
@@ -2,17 +2,31 @@
 /**
 * Class inserts neccery code for Toolbar	
 */
-class Zend_View_Helper_Pagination extends Zend_View_Helper_Abstract{
+class Zend_View_Helper_Pagination extends Zend_View_Helper_Abstract
+{
+	public function Pagination(): string
+	{
+		$p = (array)($this->view->pagination ?? []);
 
-	public function Pagination() { ?>
-		<div id="pagination" class="toolbar">
-			<span>Angezeigt: <?php echo $this->view->pagination['count']; ?></span>
-			<span>(<?php echo $this->view->pagination['start']; ?>:<?php echo $this->view->pagination['end']; ?>)</span>
-			<span>|</span>
-			<span>Insgesamt: <?php echo $this->view->pagination['records']; ?></span>
-			<span>|</span>
-			<span>Seite:<?php echo $this->view->toolbar->page; ?></span>
-		</div>
-		<?php
+		$count = (int)($p['count'] ?? 0);
+		$start = (int)($p['start'] ?? 0);
+		$end = (int)($p['end'] ?? 0);
+		$records = (int)($p['records'] ?? 0);
+
+		$pageSelect = '';
+		if (isset($this->view->toolbar) && $this->view->toolbar instanceof DEEC_Form) {
+			$pageSelect = $this->view->toolbar->renderElement('page');
+		}
+
+		return
+			'<div id="pagination" class="toolbar">'
+				. '<span>Angezeigt: ' . $count . '</span>'
+				. '<span>(' . $start . ':' . $end . ')</span>'
+				. '<span>|</span>'
+				. '<span>Insgesamt: ' . $records . '</span>'
+				. '<span>|</span>'
+				. '<span>Seite:' . $pageSelect . '</span>'
+			. '</div>';
+		return '';
 	}
 }
