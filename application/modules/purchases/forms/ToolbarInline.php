84ef47cb092994826d7972629ead81328d40cb00
kseones <remzi@dewawi.com>
2026-04-03 17:30:01 +0200

    added inline toolbar forms across modules

---
+++ application/modules/purchases/forms/ToolbarInline.php
@@ -0,0 +1,42 @@
+<?php
+
+class Purchases_Form_ToolbarInline extends DEEC_Form
+{
+	public function __construct()
+	{
+		$this->addElement([
+			'type' => 'button',
+			'name' => 'view',
+			'attribs'=> ['class' => 'view nolabel'],
+			'wrap'=> false,
+		]);
+
+		$this->addElement([
+			'type' => 'button',
+			'name' => 'edit',
+			'attribs'=> ['class' => 'edit nolabel'],
+			'wrap'=> false,
+		]);
+
+		$this->addElement([
+			'type' => 'button',
+			'name' => 'copy',
+			'attribs'=> ['class' => 'copy nolabel'],
+			'wrap'=> false,
+		]);
+
+		$this->addElement([
+			'type' => 'button',
+			'name' => 'delete',
+			'attribs'=> ['class' => 'delete nolabel'],
+			'wrap'=> false,
+		]);
+
+		$this->addElement([
+			'type' => 'button',
+			'name' => 'pdf',
+			'attribs'=> ['class' => 'pdf nolabel'],
+			'wrap'=> false,
+		]);
+	}
+}
