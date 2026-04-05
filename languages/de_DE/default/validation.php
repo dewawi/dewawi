d23b26097c16306da3007ef3c09191792496447a
kseones <remzi@dewawi.com>
2026-04-05 12:11:07 +0200

    replace Zend_Validate language file with new validation translations

---
+++ languages/de_DE/default/validation.php
@@ -0,0 +1,9 @@
+<?php return array(
+    'VALIDATION_REQUIRED' => 'Dieses Feld ist erforderlich.',
+    'VALIDATION_EMAIL' => 'Bitte geben Sie eine gültige E-Mail-Adresse ein.',
+    'VALIDATION_NUMBER' => 'Bitte geben Sie eine Zahl ein.',
+    'VALIDATION_MIN' => 'Der eingegebene Wert ist zu klein.',
+    'VALIDATION_MAX' => 'Der eingegebene Wert ist zu groß.',
+    'VALIDATION_PATTERN' => 'Das Format ist ungültig.',
+    'VALIDATION_INVALID' => 'Ungültige Eingabe.',
+);
