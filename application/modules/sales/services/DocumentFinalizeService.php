18d18769cf7ad36144ee225a6c762907b95f2887
kseones <remzi@dewawi.com>
2026-04-05 09:34:07 +0200

    add document payload and finalize services for unified document handling

---
+++ application/modules/sales/services/DocumentFinalizeService.php
@@ -0,0 +1,39 @@
+<?php
+
+class Sales_Service_DocumentFinalizeService
+{
+	public function finalize(array $document, string $controller): array
+	{
+		$docIdField = $controller . 'id';
+
+		if (!empty($document[$docIdField])) {
+			return $document;
+		}
+
+		$incrementDb = new Application_Model_DbTable_Increment();
+		$increment = $incrementDb->getIncrement($docIdField);
+
+		$filenameDb = new Application_Model_DbTable_Filename();
+		$filename = $filenameDb->getFilename($controller, $document['language']);
+		$filename = str_replace('%NUMBER%', $increment, $filename);
+
+		$documentDb = $this->getDocumentDb($controller);
+
+		$values = [
+			$docIdField => $increment,
+			'filename' => $filename,
+			'state' => 105,
+		];
+
+		$documentDb->updateById($document['id'], $values);
+		$incrementDb->setIncrement($increment, $docIdField);
+
+		return $documentDb->getById($document['id']);
+	}
+
+	protected function getDocumentDb(string $controller)
+	{
+		$class = 'Sales_Model_DbTable_' . ucfirst($controller);
+		return new $class();
+	}
+}
