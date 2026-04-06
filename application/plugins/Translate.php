492133d45438eef480702351a1238a48a329fb39
kseones <remzi@dewawi.com>
2026-04-06 10:32:42 +0200

    refactor translate plugin to use new translation library with module common loading

--- application/plugins/Translate.php
+++ application/plugins/Translate.php
@@ -4,51 +4,75 @@ class Application_Plugin_Translate extends Zend_Controller_Plugin_Abstract
 {
 	public function routeShutdown(Zend_Controller_Request_Abstract $request)
 	{
-		$params = $request->getParams();
-		$module = $params['module'];
-		$language = Zend_Registry::get('Zend_Locale');
+		if (!Zend_Registry::isRegistered('DEEC_Translate')) {
+			return;
+		}
 
-		//Translate
-		$translate = new Zend_Translate('array', BASE_PATH.'/languages/'.$language);
-		Zend_Registry::set('Zend_Translate', $translate);
-		//error_log($language);
+		$tr = Zend_Registry::get('DEEC_Translate');
+		if (!$tr instanceof DEEC_Translate) {
+			return;
+		}
 
-		//Get view instance
-		$view = Zend_Controller_Front::getInstance()
-						->getParam('bootstrap')
-						->getResource('view');
+		$localeCode = trim((string)$tr->getLocale());
+		if ($localeCode === '') {
+			return;
+		}
 
-		if(Zend_Registry::isRegistered('User')) {
-			$user = Zend_Registry::get('User');
-			//if($user['admin']) {
-				//Get languages
-				$languageDb = new Application_Model_DbTable_Language();
-				$languages = $languageDb->getLanguages();
+		$base = BASE_PATH . '/languages/' . $localeCode;
 
-				//Language switcher
-				if(count($languages) > 1) {
-					$form = new Application_Form_Language();
-					$form->language->addMultiOptions($languages);
-					$authNamespace = new Zend_Session_Namespace('Zend_Auth');
-					if(isset($authNamespace->storage->language) && $authNamespace->storage->language) {
-						$form->language->setValue($authNamespace->storage->language);
-					}
-					$view->languageSwitcher = $form->getElement('language');
+		// Load common.php from every module directory
+		if (is_dir($base)) {
+			$dirs = scandir($base);
+
+			foreach ($dirs as $dirName) {
+				if ($dirName === '.' || $dirName === '..' || $dirName === 'default') {
+					continue;
 				}
-			//}
 
-			$view->language = $language;
+				$moduleDir = $base . '/' . $dirName;
+				if (!is_dir($moduleDir)) {
+					continue;
+				}
 
-			//Translate module specific
-			/*if($module == "default") {
-				$translate = new Zend_Translate('array', BASE_PATH.'/languages/'.$language);
-			} else {
-				$translate = new Zend_Translate('array', BASE_PATH.'/languages/'.$language.'/default');
-				$translateModule = new Zend_Translate('array', BASE_PATH.'/languages/'.$language.'/'.$module);
-				$translate->addTranslation($translateModule);
+				$commonFile = $moduleDir . '/common.php';
+				if (is_file($commonFile)) {
+					$tr->load($dirName . '_common', $commonFile);
+				}
 			}
-			Zend_Registry::set('Zend_Translate', $translate);
-			print_r($translate);*/
+		}
+
+		// Load active module files
+		$module = trim((string)$request->getModuleName());
+		if ($module !== '') {
+			$dir = $base . '/' . $module;
+			if (is_dir($dir)) {
+				$tr->loadDir($module, $dir);
+			}
+		}
+
+		$view = Zend_Controller_Front::getInstance()
+			->getParam('bootstrap')
+			->getResource('view');
+
+		$view->language = Zend_Registry::get('Zend_Locale');
+
+		if (!Zend_Registry::isRegistered('User')) {
+			return;
+		}
+
+		$languageDb = new Application_Model_DbTable_Language();
+		$languages = $languageDb->getLanguages();
+
+		if (count($languages) > 1) {
+			$form = new Application_Form_Language();
+			$form->language->addMultiOptions($languages);
+
+			$authNamespace = new Zend_Session_Namespace('Zend_Auth');
+			if (isset($authNamespace->storage->language) && $authNamespace->storage->language) {
+				$form->language->setValue($authNamespace->storage->language);
+			}
+
+			$view->languageSwitcher = $form->getElement('language');
 		}
 	}
 }
