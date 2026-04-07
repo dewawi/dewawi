a2a64fd2b9eb27f5d92b735f88ab0a1146fca237
kseones <remzi@dewawi.com>
2026-04-07 16:21:34 +0200

    refactor contacts.js with helper functions json handling and improved form synchronization

--- js/contacts.js
+++ js/contacts.js
@@ -1,126 +1,140 @@
-$(document).ready(function(){
-	//Contact
-	//$(document).on('blur', '#address input', function() {
-	//	var formElementId = $(this).attr('name');
-	//	validateAddress(formElementId);
-	//});
-});
-
-//Apply Contact
 function applyContact(contactID) {
-	var data = {};
 	var contact = getContact(contactID);
+	var data = {};
 
-	$('#tabOverview #contactid').val(contact['contactid']);
-	$('#tabCustomer #contactid').val(contact['contactid']);
-	$('#tabOverview #customerid').val(contact['contactid']);
-	$('#tabCustomer #customerid').val(contact['contactid']);
-	$('#tabOverview #billingname1').val(contact['name1']);
-	$('#tabCustomer #billingname1').val(contact['name1']);
-	$('#billingname2').val(contact['name2']);
-	$('#billingstreet').val(contact['street']);
-	$('#billingdepartment').val(contact['department']);
-	$('#billingpostcode').val(contact['postcode']);
-	$('#billingcity').val(contact['city']);
-	$('#billingcountry').val(contact['country']);
-	$('#taxfree').val(contact['taxfree']);
-	$('#contactinfo').val(contact['info']);
-
-	data['contactid'] = contact['contactid'];
-	data['billingname1'] = contact['name1'];
-	data['billingname2'] = contact['name2'];
-	data['billingstreet'] = contact['street'];
-	data['billingdepartment'] = contact['department'];
-	data['billingpostcode'] = contact['postcode'];
-	data['billingcity'] = contact['city'];
-	data['billingcountry'] = contact['country'];
-	data['taxfree'] = contact['taxfree'];
-
-	if(contact['shippingname1']) {
-		$('#shippingname1').val(contact['shippingname1']);
-		$('#shippingname2').val(contact['shippingname2']);
-		$('#shippingdepartment').val(contact['shippingdepartment']);
-		$('#shippingstreet').val(contact['shippingstreet']);
-		$('#shippingpostcode').val(contact['shippingpostcode']);
-		$('#shippingcity').val(contact['shippingcity']);
-		$('#shippingcountry').val(contact['shippingcountry']);
-		$('#shippingphone').val(contact['shippingphone']);
-
-		data['shippingname1'] = contact['shippingname1'];
-		data['shippingname2'] = contact['shippingname2'];
-		data['shippingdepartment'] = contact['shippingdepartment'];
-		data['shippingstreet'] = contact['shippingstreet'];
-		data['shippingpostcode'] = contact['shippingpostcode'];
-		data['shippingcity'] = contact['shippingcity'];
-		data['shippingcountry'] = contact['shippingcountry'];
-		data['shippingphone'] = contact['shippingphone'];
-	} else if(controller == 'deliveryorder') {
-		$('#shippingname1').val(contact['name1']);
-		$('#shippingname2').val(contact['name2']);
-		$('#shippingdepartment').val(contact['department']);
-		$('#shippingstreet').val(contact['street']);
-		$('#shippingpostcode').val(contact['postcode']);
-		$('#shippingcity').val(contact['city']);
-		$('#shippingcountry').val(contact['country']);
-
-		data['shippingname1'] = contact['name1'];
-		data['shippingname2'] = contact['name2'];
-		data['shippingdepartment'] = contact['department'];
-		data['shippingstreet'] = contact['street'];
-		data['shippingpostcode'] = contact['postcode'];
-		data['shippingcity'] = contact['city'];
-		data['shippingcountry'] = contact['country'];
+	if (!contact) {
+		return;
 	}
 
-	//Refresh contact data
-	$('#phones').empty();
-	$('#emails').empty();
-	$('#internets').empty();
-	if(contact['phones']) {
-		var phones = contact['phones'].split(',');
-		for(i = 0; typeof phones[i] !== 'undefined'; i++)
-			$('#phones').append('<label>'+phones[i]+'</label><br>');
-	}
-	if(contact['emails']) {
-		var emails = contact['emails'].split(',');
-		for(i = 0; typeof emails[i] !== 'undefined'; i++)
-			$('#emails').append('<label>'+emails[i]+'</label><br>');
-	}
-	if(contact['internets']) {
-		var internets = contact['internets'].split(',');
-		for(i = 0; typeof internets[i] !== 'undefined'; i++)
-			$('#internets').append('<label>'+internets[i]+'</label><br>');
+	setValue('#taboverview #contactid, #tabcustomer #contactid', contact.contactid);
+	setValue('#taboverview #customerid, #tabcustomer #customerid', contact.contactid);
+
+	setValue('#taboverview #billingname1, #tabcustomer #billingname1', contact.name1);
+	setValue('#taboverview #billingname2, #tabcustomer #billingname2', contact.name2);
+	setValue('#taboverview #billingstreet, #tabcustomer #billingstreet', contact.street);
+	setValue('#taboverview #billingdepartment, #tabcustomer #billingdepartment', contact.department);
+	setValue('#taboverview #billingpostcode, #tabcustomer #billingpostcode', contact.postcode);
+	setValue('#taboverview #billingcity, #tabcustomer #billingcity', contact.city);
+	setValue('#taboverview #billingcountry, #tabcustomer #billingcountry', contact.country);
+	setCheckbox('#taboverview #taxfree, #tabcustomer #taxfree', contact.taxfree);
+	setValue('#contactinfo', contact.info);
+
+	data.contactid = contact.contactid;
+	data.billingname1 = contact.name1;
+	data.billingname2 = contact.name2;
+	data.billingstreet = contact.street;
+	data.billingdepartment = contact.department;
+	data.billingpostcode = contact.postcode;
+	data.billingcity = contact.city;
+	data.billingcountry = contact.country;
+	data.taxfree = contact.taxfree ? 1 : 0;
+
+	if (contact.shippingname1) {
+		setValue('#shippingname1', contact.shippingname1);
+		setValue('#shippingname2', contact.shippingname2);
+		setValue('#shippingdepartment', contact.shippingdepartment);
+		setValue('#shippingstreet', contact.shippingstreet);
+		setValue('#shippingpostcode', contact.shippingpostcode);
+		setValue('#shippingcity', contact.shippingcity);
+		setValue('#shippingcountry', contact.shippingcountry);
+		setValue('#shippingphone', contact.shippingphone);
+
+		data.shippingname1 = contact.shippingname1;
+		data.shippingname2 = contact.shippingname2;
+		data.shippingdepartment = contact.shippingdepartment;
+		data.shippingstreet = contact.shippingstreet;
+		data.shippingpostcode = contact.shippingpostcode;
+		data.shippingcity = contact.shippingcity;
+		data.shippingcountry = contact.shippingcountry;
+		data.shippingphone = contact.shippingphone;
+	} else if (controller === 'deliveryorder') {
+		setValue('#shippingname1', contact.name1);
+		setValue('#shippingname2', contact.name2);
+		setValue('#shippingdepartment', contact.department);
+		setValue('#shippingstreet', contact.street);
+		setValue('#shippingpostcode', contact.postcode);
+		setValue('#shippingcity', contact.city);
+		setValue('#shippingcountry', contact.country);
+
+		data.shippingname1 = contact.name1;
+		data.shippingname2 = contact.name2;
+		data.shippingdepartment = contact.department;
+		data.shippingstreet = contact.street;
+		data.shippingpostcode = contact.postcode;
+		data.shippingcity = contact.city;
+		data.shippingcountry = contact.country;
 	}
 
-	//Close the modal window
+	refreshContactInfoList('#phones', contact.phones);
+	refreshContactInfoList('#emails', contact.emails);
+	refreshContactInfoList('#internets', contact.internets);
+
 	modalWindowClose();
 
-	if((module == 'processes') || (module == 'tasks')) {
-		data['customerid'] = data['contactid'];
+	if (module === 'processes' || module === 'tasks') {
+		data.customerid = data.contactid;
 		delete data.contactid;
 	}
 
 	isDirty = true;
-	//console.log(data);
 	edit(data);
 
-	//Refresh file manager
-	$('#tabFiles iframe').attr('src', function (i, val) { return val; });
-	$('#tabFiles #messages').hide();
-	$('#tabFiles iframe').show();
+	$('#tabfiles iframe').each(function () {
+		$(this).attr('src', $(this).attr('src'));
+	});
+
+	$('#tabfiles #messages').hide();
+	$('#tabfiles iframe').show();
 }
 
-//Get contact data
-function getContact(contactID){
-	var data;
+function setValue(selector, value) {
+	$(selector).val(value != null ? value : '');
+}
+
+function setCheckbox(selector, value) {
+	$(selector).prop('checked', Number(value) === 1);
+}
+
+function refreshContactInfoList(selector, value) {
+	var $target = $(selector);
+
+	if (!$target.length) {
+		return;
+	}
+
+	$target.empty();
+
+	if (!value) {
+		return;
+	}
+
+	var items = String(value).split(',');
+
+	for (var i = 0; i < items.length; i++) {
+		var item = $.trim(items[i]);
+
+		if (item !== '') {
+			$target.append('<label>' + item + '</label><br>');
+		}
+	}
+}
+
+function getContact(contactID) {
+	var data = null;
+
 	$.ajax({
 		type: 'POST',
 		async: false,
-		url: baseUrl+'/contacts/contact/get/id/'+contactID,
+		url: baseUrl + '/contacts/contact/get/id/' + contactID,
 		cache: false,
-		success: function(response){
+		dataType: 'json',
+		success: function (response) {
 			data = response;
+		},
+		error: function (xhr) {
+			console.log('getContact failed', xhr.responseText);
 		}
 	});
+
 	return data;
 }
