cb7a214d9af734262fa841d7e2943cf3de0e702b
kseones <remzi@dewawi.com>
2026-04-07 09:21:09 +0200

    migrate upload form to new form system and simplify element definition

--- application/forms/Upload.php
+++ application/forms/Upload.php
@@ -1,25 +1,31 @@
 <?php
 
-class Application_Form_Upload extends Zend_Form
+class Application_Form_Upload extends DEEC_Form
 {
-	public function init()
+	public function __construct()
 	{
-		$this->setName('upload');
-		$this->setAttrib('enctype', 'multipart/form-data');
+		$this->addElement([
+			'name' => 'file',
+			'type' => 'file',
+			'label' => 'FILE',
+			'required' => true,
+			'attribs' => [
+				'name' => 'file[]',
+				'id' => 'file',
+				'multiple' => 'multiple',
+				'accept' => '.pdf,.jpg,.jpeg,.png,.gif,.csv,.zip',
+			],
+		]);
 
-		$file = new Zend_Form_Element_File('file');
-		$file->setLabel('FILE')
-			->setAttrib('name', 'file[]')
-			->setAttrib('multiple', 'multiple')
-			->setRequired(true)
-			->addValidator('Count', false, 10)
-			->addValidator('Size', false, 10240000)
-			->addValidator('Extension', false, 'pdf,jpg,jpeg,png,gif,csv,zip');
-
-		$submit = new Zend_Form_Element_Submit('submit');
-		$submit->setLabel('UPLOAD_SUBMIT')
-			->setAttrib('id', 'submitbutton');
-
-		$this->addElements(array($file, $submit));
+		$this->addElement([
+			'name' => 'submit',
+			'type' => 'submit',
+			'label' => 'UPLOAD_SUBMIT',
+			'attribs' => [
+				'id' => 'submitbutton',
+				'class' => 'dw-btn dw-btn--primary',
+			],
+			'wrap' => false,
+		]);
 	}
 }
