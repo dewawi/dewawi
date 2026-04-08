7092564e500b3ec23f3f08252bedbe08521729ea
kseones <remzi@dewawi.com>
2026-04-08 22:19:47 +0200

    add processes services and dynamic position handling

---
+++ application/modules/processes/services/CreateDataFactory.php
@@ -0,0 +1,106 @@
+<?php
+
+class Processes_Service_CreateDataFactory
+{
+	public function build(string $controller, int $contactId = 0): array
+	{
+		$data = $this->getDefaults($controller);
+
+		if ($contactId > 0) {
+			$data = array_merge($data, $this->getContactData($contactId));
+		}
+
+		return $data;
+	}
+
+	protected function getDefaults(string $controller): array
+	{
+		$currencies = new Application_Model_DbTable_Currency();
+		$languages = new Application_Model_DbTable_Language();
+		$templates = new Application_Model_DbTable_Template();
+
+		$currency = $currencies->getPrimaryCurrency();
+		$language = $languages->getPrimaryLanguage();
+		$template = $templates->getPrimaryTemplate();
+
+		return [
+			'title' => $this->getDefaultTitle($controller),
+			'currency' => $currency['code'],
+			'templateid' => $template['id'],
+			'language' => $language['code'],
+			'state' => 100,
+		];
+	}
+
+	protected function getContactData(int $contactId): array
+	{
+		$data = [];
+
+		$contactDb = new Contacts_Model_DbTable_Contact();
+		$contact = $contactDb->getContact($contactId);
+
+		if (!$contact) {
+			return $data;
+		}
+
+		$data['contactid'] = $contact['contactid'];
+		$data['billingname1'] = $contact['name1'];
+		$data['billingname2'] = $contact['name2'];
+		$data['billingdepartment'] = $contact['department'];
+
+		$addressDb = new Contacts_Model_DbTable_Address();
+		$addresses = $addressDb->getAddress($contact['id']);
+
+		if (count($addresses)) {
+			$data['billingstreet'] = $addresses[0]['street'];
+			$data['billingpostcode'] = $addresses[0]['postcode'];
+			$data['billingcity'] = $addresses[0]['city'];
+			$data['billingcountry'] = $addresses[0]['country'];
+		}
+
+		if (!empty($contact['vatin'])) {
+			$data['vatin'] = $contact['vatin'];
+		}
+
+		if (!empty($contact['currency'])) {
+			$data['currency'] = $contact['currency'];
+		}
+
+		if (!empty($contact['taxfree'])) {
+			$data['taxfree'] = $contact['taxfree'];
+		}
+
+		return $data;
+	}
+
+	protected function getDefaultTitle(string $controller): string
+	{
+		$map = [
+			'process' => $this->translate('QUOTES_NEW_PROCESS'),
+		];
+
+		return $map[$controller] ?? strtoupper($controller) . ' NEW';
+	}
+
+	protected function getTranslator(): ?DEEC_Translate
+	{
+		if (Zend_Registry::isRegistered('DEEC_Translate')) {
+			$translator = Zend_Registry::get('DEEC_Translate');
+			if ($translator instanceof DEEC_Translate) {
+				return $translator;
+			}
+		}
+
+		return null;
+	}
+
+	protected function translate(string $key): string
+	{
+		$translator = $this->getTranslator();
+		if ($translator) {
+			return $translator->t($key);
+		}
+
+		return $key;
+	}
+}
---
+++ application/modules/processes/services/EmailFormFactory.php
@@ -0,0 +1,89 @@
+<?php
+
+class Processes_Service_EmailFormFactory
+{
+	public function build(array $document, array $contact, string $controller): Contacts_Form_Emailmessage
+	{
+		$emailForm = new Contacts_Form_Emailmessage();
+
+		$emailDb = new Contacts_Model_DbTable_Email();
+		$contactpersonDb = new Contacts_Model_DbTable_Contactperson();
+		$emailtemplateDb = new Contacts_Model_DbTable_Emailtemplate();
+
+		$recipientOptions = [];
+
+		// Contact emails
+		$contactEmails = $emailDb->getByParentId((int) $contact['id'], 'contacts', 'contact');
+		foreach ($contactEmails as $row) {
+			if (empty($row['id']) || empty($row['email'])) {
+				continue;
+			}
+
+			$recipientOptions[(string) $row['id']] = (string) $row['email'];
+		}
+
+		// Contact person emails
+		$contactpersons = $contactpersonDb->getByParentId((int) $contact['id'], 'contacts', 'contact');
+		foreach ($contactpersons as $contactperson) {
+			$personEmails = $emailDb->getByParentId((int) $contactperson['id'], 'contacts', 'contactperson');
+
+			foreach ($personEmails as $row) {
+				if (empty($row['id']) || empty($row['email'])) {
+					continue;
+				}
+
+				$personName = trim(((string) ($contactperson['name1'] ?? '')) . ' ' . ((string) ($contactperson['name2'] ?? '')));
+				$label = (string) $row['email'];
+
+				if ($personName !== '') {
+					$label .= ' (' . $personName . ')';
+				}
+
+				$recipientOptions[(string) $row['id']] = $label;
+			}
+		}
+
+		$emailForm->addOptions('recipient', $recipientOptions, 'merge');
+
+		// Template key = controller
+		$emailtemplate = $emailtemplateDb->getEmailtemplate('processes', $controller);
+		if ($emailtemplate) {
+			if (!empty($emailtemplate['cc'])) {
+				$emailForm->setValue('cc', (string) $emailtemplate['cc']);
+			}
+
+			if (!empty($emailtemplate['bcc'])) {
+				$emailForm->setValue('bcc', (string) $emailtemplate['bcc']);
+			}
+
+			if (!empty($emailtemplate['replyto'])) {
+				$emailForm->setValue('replyto', (string) $emailtemplate['replyto']);
+			}
+
+			$docIdField = $this->getDocumentIdField($controller);
+
+			$search = ['[DOCID]', '[CONTACTID]'];
+			$replace = [
+				(string) ($document[$docIdField] ?? ''),
+				(string) ($document['contactid'] ?? ''),
+			];
+
+			$emailForm->setValue(
+				'subject',
+				str_replace($search, $replace, (string) ($emailtemplate['subject'] ?? ''))
+			);
+
+			$emailForm->setValue(
+				'body',
+				str_replace($search, $replace, (string) ($emailtemplate['body'] ?? ''))
+			);
+		}
+
+		return $emailForm;
+	}
+
+	protected function getDocumentIdField(string $controller): string
+	{
+		return $controller . 'id';
+	}
+}
