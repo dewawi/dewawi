fa59dd472cdcf14af19907d068b7f1b3309fb816
kseones <remzi@dewawi.com>
2026-04-08 21:03:31 +0200

    add directory parameter to file manager iframe

--- application/modules/sales/views/helpers/Files.php
+++ application/modules/sales/views/helpers/Files.php
@@ -6,8 +6,12 @@ class Zend_View_Helper_Files extends Zend_View_Helper_Abstract
 {
 	public function Files()
 	{
-		return '<iframe src="' 
-			. $this->view->baseUrl() 
-			. '/library/FileManager/dialog.php?lang=de&type=0" width="100%" height="700px"></iframe>';
+		$directory = trim((string)($this->view->contactUrl ?? ''));
+
+		return '<iframe src="'
+			. $this->view->baseUrl()
+			. '/library/FileManager/dialog.php?lang=de&type=0&directory='
+			. $directory
+			. '" width="100%" height="700px"></iframe>';
 	}
 }
