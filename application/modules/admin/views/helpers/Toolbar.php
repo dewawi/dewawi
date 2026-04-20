32364a7933af160504ab39fe961dc7320848cf10
kseones <remzi@dewawi.com>
2026-04-20 16:18:33 +0200

    add safety checks and unify rendering in admin toolbar helpers

--- application/modules/admin/views/helpers/Toolbar.php
+++ application/modules/admin/views/helpers/Toolbar.php
@@ -4,36 +4,42 @@ class Zend_View_Helper_Toolbar extends Zend_View_Helper_Abstract
 {
 	public function Toolbar()
 	{
+		$view = $this->view;
+		$toolbar = $view->toolbar;
 		$html = '';
 
-		if ($this->view->action === 'edit') {
-			$html .= '<input class="id" type="hidden" value="' . $this->view->escape($this->view->id) . '" name="id" />';
-			$html .= $this->view->toolbar->renderElement('copy');
-			$html .= $this->view->toolbar->renderElement('delete');
+		if (!$toolbar || !is_object($toolbar) || !method_exists($toolbar, 'renderElement')) {
+			return '';
+		}
+
+		if ($view->action === 'edit') {
+			$html .= '<input class="id" type="hidden" value="' . $view->escape($view->id) . '" name="id" />';
+			$html .= $toolbar->renderElement('copy');
+			$html .= $toolbar->renderElement('delete');
 
 			return $html;
 		}
 
 		if (
-			$this->view->controller !== 'index'
-			&& $this->view->controller !== 'media'
-			&& $this->view->controller !== 'export'
+			$view->controller !== 'index'
+			&& $view->controller !== 'media'
+			&& $view->controller !== 'export'
 		) {
-			if (!empty($this->view->user['admin'])) {
-				$html .= $this->view->toolbar->renderElement('copy');
-				$html .= $this->view->toolbar->renderElement('delete');
+			if (!empty($view->user['admin'])) {
+				$html .= $toolbar->renderElement('copy');
+				$html .= $toolbar->renderElement('delete');
 			}
 
 			if (
-				$this->view->controller === 'category'
-				|| $this->view->controller === 'page'
-				|| $this->view->controller === 'tag'
+				$view->controller === 'category'
+				|| $view->controller === 'page'
+				|| $view->controller === 'tag'
 			) {
-				$html .= $this->view->toolbar->renderElement('type');
+				$html .= $toolbar->renderElement('type');
 			}
 
-			if ($this->view->toolbar->getValue('type') === 'shop') {
-				$html .= $this->view->toolbar->renderElement('shopid');
+			if ($toolbar->getValue('type') === 'shop') {
+				$html .= $toolbar->renderElement('shopid');
 			}
 		}
 
