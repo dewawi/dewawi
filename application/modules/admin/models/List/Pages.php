ea468d12750f30a50faf61f9adea2114feabdda8
kseones <remzi@dewawi.com>
2026-05-01 09:54:13 +0200

    add reusable admin list models and shared list view

---
+++ application/modules/admin/models/List/Pages.php
@@ -0,0 +1,101 @@
+<?php
+
+class Admin_Model_List_Pages extends DEEC_List
+{
+	public function __construct()
+	{
+		$this->init();
+	}
+
+	public function init(): void
+	{
+		$this->setId('pages');
+
+		$this->setRowClassCallback(function ($item) {
+			return !empty($item->pinned) ? 'is-pinned' : '';
+		});
+
+		$this->setColumns($this->buildColumns());
+	}
+
+	protected function buildColumns()
+	{
+		return [
+			[
+				'name' => 'id',
+				'label' => 'ADMIN_PAGE_ID',
+				'data_label' => 'ADMIN_PAGE_ID',
+				'type' => 'link',
+				'field' => 'id',
+				'url' => [
+					'action' => 'edit',
+					'id_field' => 'id',
+				],
+				'class' => 'dw-col-id',
+				'empty_hide' => true,
+			],
+			[
+				'name' => 'title',
+				'label' => 'ADMIN_TITLE',
+				'data_label' => 'ADMIN_TITLE',
+				'type' => 'link',
+				'field' => 'title',
+				'fallback_field' => 'id',
+				'url' => [
+					'action' => 'edit',
+					'id_field' => 'id',
+				],
+				'class' => 'dw-col-title',
+			],
+			[
+				'name' => 'subtitle',
+				'label' => 'ADMIN_SUBTITLE',
+				'data_label' => 'ADMIN_SUBTITLE',
+				'type' => 'link',
+				'field' => 'subtitle',
+				'fallback_field' => 'id',
+				'url' => [
+					'action' => 'edit',
+					'id_field' => 'id',
+				],
+				'class' => 'dw-col-subtitle',
+			],
+			[
+				'name' => 'parentid',
+				'label' => 'ADMIN_MAIN_PAGE',
+				'data_label' => 'ADMIN_MAIN_PAGE',
+				'type' => 'text',
+				'field' => 'subtitle',
+				'fallback_field' => 'id',
+				'class' => 'dw-col-parentid',
+			],
+			[
+				'name' => 'actions',
+				'label' => '',
+				'type' => 'actions',
+				'class' => 'dw-col-actions',
+				'elements' => [
+					[
+						'name' => 'edit',
+						'show' => function ($page) {
+							return $this->hasPermission('admin');
+						},
+					],
+					[
+						'name' => 'copy',
+						'show' => function ($page) {
+							return $this->hasPermission('admin');
+						},
+					],
+					[
+						'name' => 'delete',
+						'show' => function ($page) {
+							return $this->hasPermission('admin');
+						},
+					],
+					['name' => 'view'],
+				],
+			],
+		];
+	}
+}
