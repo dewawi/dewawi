283e693ac98771aabe62474963d1866405f486ef
kseones <remzi@dewawi.com>
2026-05-02 21:44:51 +0200

    add missing inline toolbar forms

---
+++ application/modules/tasks/forms/ToolbarInline.php
@@ -0,0 +1,35 @@
+<?php
+
+class Tasks_Form_ToolbarInline extends DEEC_Form
+{
+	public function __construct()
+	{
+		$this->addElement([
+			'type' => 'button',
+			'name' => 'view',
+			'attribs' => ['class' => 'view nolabel'],
+			'wrap' => false,
+		]);
+
+		$this->addElement([
+			'type' => 'button',
+			'name' => 'edit',
+			'attribs' => ['class' => 'edit nolabel'],
+			'wrap' => false,
+		]);
+
+		$this->addElement([
+			'type' => 'button',
+			'name' => 'copy',
+			'attribs' => ['class' => 'copy nolabel'],
+			'wrap' => false,
+		]);
+
+		$this->addElement([
+			'type' => 'button',
+			'name' => 'delete',
+			'attribs' => ['class' => 'delete nolabel'],
+			'wrap' => false,
+		]);
+	}
+}
