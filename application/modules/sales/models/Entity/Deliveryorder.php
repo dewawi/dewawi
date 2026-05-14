e5c37d5cd4a0a0dcaea7bf5ba93fdf32b807c744
kseones <remzi@dewawi.com>
2026-05-14 12:06:22 +0200

    add sales entity model classes

---
+++ application/modules/sales/models/Entity/Deliveryorder.php
@@ -0,0 +1,57 @@
+<?php
+
+class Sales_Model_Entity_Deliveryorder
+{
+	public static function listConfig(): array
+	{
+		return [
+			'tableClass' => 'Sales_Model_DbTable_Deliveryorder',
+			'alias' => 'd',
+
+			'joins' => [
+				[
+					'table' => 'contact',
+					'alias' => 'c',
+					'on' => 'd.contactid = c.contactid',
+					'columns' => [
+						'catid AS catid',
+						'id AS cid',
+					],
+				],
+			],
+
+			'search' => [
+				'title',
+				'quoteid',
+				'contactid',
+				'billingname1',
+				'billingname2',
+				'billingcity',
+				'shippingname1',
+				'shippingcity',
+			],
+
+			'filters' => [
+				'catid' => [
+					'type' => 'category',
+					'alias' => 'c',
+				],
+				'states' => [
+					'type' => 'states',
+				],
+				'country' => [
+					'type' => 'country',
+					'columns' => ['billingcountry', 'shippingcountry'],
+				],
+				'daterange' => [
+					'type' => 'daterange',
+				],
+			],
+
+			'orders' => [
+				'documentid' => 'quoteid',
+				'name1' => 'c.name1',
+			],
+		];
+	}
+}
