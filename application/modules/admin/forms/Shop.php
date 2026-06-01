c11e5683a5c472f5dc964e76a25b0dfc07fdf4fe
kseones <remzi@dewawi.com>
2026-06-01 19:19:55 +0200

    migrate shop form to library form class

--- application/modules/admin/forms/Shop.php
+++ application/modules/admin/forms/Shop.php
@@ -1,53 +1,77 @@
 <?php
 
-class Admin_Form_Shop extends Zend_Form
+class Admin_Form_Shop extends DEEC_Form
 {
-	public function init()
+	public function __construct()
 	{
-		$this->setName('shop');
+		$this->addElement([
+			'name' => 'id',
+			'type' => 'hidden',
+			'format' => ['type' => 'int'],
+			'wrap' => false,
+		]);
 
-		$form = array();
+		$this->addElement([
+			'name' => 'title',
+			'type' => 'text',
+			'label' => 'ADMIN_TITLE',
+			'format' => ['type' => 'string'],
+			'attribs' => [
+				'maxlength' => 255,
+			],
+			'col' => 12,
+		]);
 
-		$form['id'] = new Zend_Form_Element_Hidden('id');
-		$form['id']->addFilter('Int')->removeDecorator('Label');
+		$this->addElement([
+			'name' => 'url',
+			'type' => 'text',
+			'label' => 'ADMIN_URL',
+			'format' => ['type' => 'string'],
+			'attribs' => [
+				'maxlength' => 255,
+			],
+			'col' => 12,
+		]);
 
-		$form['title'] = new Zend_Form_Element_Text('title');
-		$form['title']->setLabel('ADMIN_TITLE')
-			->addFilter('StripTags')
-			->addFilter('StringTrim')
-			->setAttrib('size', '12');
+		$this->addElement([
+			'name' => 'logo',
+			'type' => 'text',
+			'label' => 'ADMIN_LOGO',
+			'format' => ['type' => 'string'],
+			'attribs' => [
+				'maxlength' => 255,
+			],
+			'col' => 12,
+		]);
 
-		$form['url'] = new Zend_Form_Element_Text('url');
-		$form['url']->setLabel('ADMIN_URL')
-			->addFilter('StripTags')
-			->addFilter('StringTrim')
-			->setAttrib('size', '12');
+		$this->addElement([
+			'name' => 'footer',
+			'type' => 'text',
+			'label' => 'ADMIN_FOOTER',
+			'format' => ['type' => 'string'],
+			'attribs' => [
+				'maxlength' => 255,
+			],
+			'col' => 12,
+		]);
 
-		$form['logo'] = new Zend_Form_Element_Text('logo');
-		$form['logo']->setLabel('ADMIN_LOGO')
-			->addFilter('StripTags')
-			->addFilter('StringTrim')
-			->setAttrib('size', '12');
+		$this->addElement([
+			'name' => 'emailsender',
+			'type' => 'text',
+			'label' => 'ADMIN_EMAIL',
+			'format' => ['type' => 'string'],
+			'attribs' => [
+				'maxlength' => 255,
+			],
+			'col' => 12,
+		]);
 
-		$form['footer'] = new Zend_Form_Element_Text('footer');
-		$form['footer']->setLabel('ADMIN_FOOTER')
-			->addFilter('StripTags')
-			->addFilter('StringTrim')
-			->setAttrib('size', '12');
-
-		$form['emailsender'] = new Zend_Form_Element_Text('emailsender');
-		$form['emailsender']->setLabel('ADMIN_EMAIL')
-			->addFilter('StripTags')
-			->addFilter('StringTrim')
-			->setAttrib('size', '12');
-
-		$form['activated'] = new Zend_Form_Element_Checkbox('activated');
-		$form['activated']->addFilter('Int')->removeDecorator('Label');
-
-		//$form['language'] = new Zend_Form_Element_Select('language');
-		//$form['language']->setDecorators(array('ViewHelper'))
-		//	->setAttrib('default', '');
-
-		$this->addElements($form);
+		$this->addElement([
+			'name' => 'activated',
+			'type' => 'checkbox',
+			'label' => 'ADMIN_ACTIVATED',
+			'format' => ['type' => 'int'],
+			'col' => 12,
+		]);
 	}
 }
