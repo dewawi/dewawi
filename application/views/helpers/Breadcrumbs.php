9fdb21a601488d670c077644187b75cd581d6ab6
kseones <remzi@dewawi.com>
2026-06-15 13:01:43 +0200

    improve breadcrumb url generation

--- application/views/helpers/Breadcrumbs.php
+++ application/views/helpers/Breadcrumbs.php
@@ -1,17 +1,50 @@
 <?php
-/**
-* Class inserts neccery code for Breadcrumbs	
-*/
-class Zend_View_Helper_Breadcrumbs extends Zend_View_Helper_Abstract{
 
-	public function Breadcrumbs() {
-		$breadcrumbs = '<span><a href="'.$this->view->url(array('module'=>'default', 'controller'=>'index'), null, TRUE).'">'.$this->view->translate('DEWAWI').'</a></span>';
-		if($this->view->module != 'default')
-			$breadcrumbs .= '<span> &raquo; </span><span><a href="'.$this->view->url(array('module'=>$this->view->module, 'controller'=>$this->view->controller), null, TRUE).'">'.$this->view->translate(strtoupper($this->view->module)).'</a></span>';
-		if($this->view->controller != 'index')
-			$breadcrumbs .= '<span> &raquo; </span><span><a href="'.$this->view->url(array('module'=>$this->view->module, 'controller'=>$this->view->controller), null, TRUE).'">'.$this->view->translate(strtoupper($this->view->controller)).'</a></span>';
-		if($this->view->action != 'index')
-			$breadcrumbs .= '<span> &raquo; </span><span>'.$this->view->translate(strtoupper($this->view->action)).'</span>';
+class Zend_View_Helper_Breadcrumbs extends Zend_View_Helper_Abstract
+{
+	public function Breadcrumbs()
+	{
+		$breadcrumbs = '<span><a href="/">'
+			. $this->view->translate('DEWAWI')
+			. '</a></span>';
+
+		if ($this->view->module !== 'default') {
+			$breadcrumbs .= '<span> &raquo; </span><span><a href="'
+				. $this->buildUrl($this->view->module, 'index')
+				. '">' . $this->view->translate(strtoupper($this->view->module)) . '</a></span>';
+		}
+
+		if ($this->view->controller !== 'index') {
+			$breadcrumbs .= '<span> &raquo; </span><span><a href="'
+				. $this->buildUrl($this->view->module, $this->view->controller)
+				. '">' . $this->view->translate(strtoupper($this->view->controller)) . '</a></span>';
+		}
+
+		if ($this->view->action !== 'index') {
+			$breadcrumbs .= '<span> &raquo; </span><span>'
+				. $this->view->translate(strtoupper($this->view->action))
+				. '</span>';
+		}
+
 		return $breadcrumbs;
+	}
+
+	protected function buildUrl($module, $controller)
+	{
+		$params = [
+			'module' => $module,
+			'controller' => $controller,
+			'action' => 'index',
+		];
+
+		$type = Zend_Controller_Front::getInstance()
+			->getRequest()
+			->getParam('type');
+
+		if ($type) {
+			$params['type'] = $type;
+		}
+
+		return $this->view->url($params, 'default', false);
 	}
 }
