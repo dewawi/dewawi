2076db987a6aa98fe589b9d89f52b28212f59043
kseones <remzi@dewawi.com>
2026-07-13 08:42:01 +0200

    centralize permission handling

--- application/plugins/Acl.php
+++ application/plugins/Acl.php
@@ -2,106 +2,193 @@
 
 class Application_Plugin_Acl extends Zend_Controller_Plugin_Abstract
 {
-	public function routeShutdown(Zend_Controller_Request_Abstract $request)
-	{
-		$redirector = Zend_Controller_Action_HelperBroker::getStaticHelper('redirector');
-		$flashMessenger = Zend_Controller_Action_HelperBroker::getStaticHelper('FlashMessenger');
-
-		$params = $request->getParams();
+	public function routeShutdown(
+		Zend_Controller_Request_Abstract $request
+	): void {
 		$auth = Zend_Auth::getInstance();
-		if($auth->hasIdentity()) {
-			$user = Zend_Registry::get('User');
 
-			//Access control
-			$acl = array(
-					'add' => array('add', 'copy', 'generate', 'send', 'upload'),
-					'edit' => array('edit', 'import', 'lock', 'unlock', 'keepalive', 'pin', 'apply', 'sort', 'save', 'cancel'),
-					'view' => array('view', 'get', 'index', 'search', 'select', 'history', 'suggest', 'export', 'preview', 'download'),
-					'delete' => array('delete')
-					);
-			if($params['controller'] == 'error') {
-				error_log('PERMIT/'.$params['module'].'/'.$params['controller'].'/'.$params['action']);
-			} elseif($params['module'] == 'admin') {
-				//if($user['admin']) {
-					//error_log('PERMIT/'.$params['module'].'/'.$params['controller'].'/'.$params['action']);
-				//} else {
-				//	error_log('NO_PERMIT/'.$params['module'].'/'.$params['controller'].'/'.$params['action']);
-				//	$flashMessenger->addMessage('MESSAGES_ACCESS_DENIED');
-				//	$redirector->gotoSimple('index', 'index', 'default');
-				//}
-			} elseif($params['controller'] == 'downloads') {
-			} else {
-				//Check for permissions
-				$permissionsDb = new Application_Model_DbTable_Permission();
-				$permissions = $permissionsDb->getPermissions();
-				if(isset($permissions[$params['module']])) {
-					$module = json_decode($permissions[$params['module']], true);
-					if(substr($params['controller'], -3) == 'pos') $params['controller'] = substr($params['controller'], 0, -3);
-					if($params['controller'] == 'position') $params['controller'] = $params['parent'];
-					if($params['controller'] == 'positionset') $params['controller'] = $params['parent'];
-					//TO DO
-					if($params['module'] == 'contacts') $params['controller'] = 'contact';
-					//TO DO
-					if($params['module'] == 'items') $params['controller'] = 'item';
-					if(strpos($params['action'], 'generate') !== false) $params['action'] = 'generate';
-					if(isset($module[$params['controller']])) {
-						//User can not add if there is no edit permission
-						if(in_array('add', $module[$params['controller']]) && !in_array('edit', $module[$params['controller']])) {
-							$key = array_search('edit', $module[$params['controller']]);
-							unset($module[$params['controller']][$key]);
-						}
-						//Put all permissions together
-						$actions = array();
-						foreach($module[$params['controller']] as $action) {
-							$actions = array_merge($actions, $acl[$action]);
-						}
-						if(in_array($params['action'], $actions)) {
-							//error_log('PERMIT/'.$params['module'].'/'.$params['controller'].'/'.$params['action']);
-						} else {
-							error_log('NO_PERMIT/'.$params['module'].'/'.$params['controller'].'/'.$params['action']);
-							error_log('NO_PERMIT'.$request->getRequestUri());
-							$flashMessenger->addMessage('MESSAGES_ACCESS_DENIED');
-							if(in_array('view', $module[$params['controller']])) $redirector->gotoSimple('index', $params['controller'], $params['module']);
-							else $redirector->gotoSimple('index', 'index', 'index');
-						}
-					} else {
-						error_log('NO_PERMIT/'.$params['module'].'/'.$params['controller'].'/'.$params['action']);
-						error_log('NO_PERMIT'.$request->getRequestUri());
-						$flashMessenger->addMessage('MESSAGES_ACCESS_DENIED');
-						$redirector->gotoSimple('index', 'index', 'default');
-					}
-				}
-				//error_log($params['module']);
-				//error_log($_SERVER['REQUEST_URI']);
-				//$flashMessenger->addMessage('MESSAGES_LOCKED');
-
-				//Check if document is locked
-				/*if(($params['module'] == 'sales') && ($params['controller'] == 'quote')) {
-					$view = Zend_Controller_Front::getInstance()
-									->getParam('bootstrap')
-									->getResource('view');
-					$viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('viewRenderer');
-					$layout = Zend_Controller_Action_HelperBroker::getStaticHelper('layout');
-					$message = $view->translate('MESSAGES_ACCESS_DENIED_%s');
-					//$message = sprintf($message, $users[$quote['locked']]);
-					if($request->isPost()) {
-						header('Content-type: application/json');
-						$viewRenderer->setNoRender();
-						$layout->disableLayout();
-						echo Zend_Json::encode(array('message' => $message));
-					} else {
-						$flashMessenger->addMessage($message);
-						//$redirector->gotoSimple('index', 'quote', 'sales');
-					}
-				}*/
-			}
-		} elseif(($params['module'] == 'shops')) {
-		} elseif(($params['module'] != 'users') && ($params['action'] != 'login')) {
-			if(isset($params['id']) && $params['id']) {
-				$redirector->gotoSimple('login', 'user', 'users', array('url' => $params['module'].'|'.$params['controller'].'|'.$params['action'].'|'.$params['id']));
-			} else {
-				$redirector->gotoSimple('login', 'user', 'users', array('url' => $params['module'].'|'.$params['controller'].'|'.$params['action']));
-			}
+		if (!$auth->hasIdentity()) {
+			$this->handleGuestRequest($request);
+			return;
 		}
+
+		$this->handleAuthenticatedRequest($request);
+	}
+
+	private function handleAuthenticatedRequest(
+		Zend_Controller_Request_Abstract $request
+	): void {
+		$module = $request->getModuleName();
+		$controller = $request->getControllerName();
+
+		if ($controller === 'error') {
+			$this->logPermit($request);
+			return;
+		}
+
+		if ($module === 'admin') {
+			return;
+		}
+
+		if ($controller === 'downloads') {
+			return;
+		}
+
+		$user = Zend_Registry::get('User');
+		$permission = new DEEC_Permission((array)$user);
+
+		/*
+		 * Preserve the current behavior:
+		 * modules without a permission column are not blocked.
+		 */
+		if (!$permission->hasModule($module)) {
+			return;
+		}
+
+		$resolvedController = $permission->resolveController($request);
+
+		if (
+			!$permission->hasController(
+				$module,
+				$resolvedController
+			)
+		) {
+			$this->denyAccess($request);
+			return;
+		}
+
+		if ($permission->hasRoutePermission($request)) {
+			return;
+		}
+
+		$this->denyAccess(
+			$request,
+			$permission->hasPermission(
+				$module,
+				$resolvedController,
+				'view'
+			),
+			$resolvedController
+		);
+	}
+
+	private function handleGuestRequest(
+		Zend_Controller_Request_Abstract $request
+	): void {
+		if ($request->getModuleName() === 'shops') {
+			return;
+		}
+
+		if ($this->isLoginRequest($request)) {
+			return;
+		}
+
+		$this->redirectToLogin($request);
+	}
+
+	private function isLoginRequest(
+		Zend_Controller_Request_Abstract $request
+	): bool {
+		return $request->getModuleName() === 'users'
+			&& $request->getControllerName() === 'user'
+			&& $request->getActionName() === 'login';
+	}
+
+	private function redirectToLogin(
+		Zend_Controller_Request_Abstract $request
+	): void {
+		$redirector = Zend_Controller_Action_HelperBroker::getStaticHelper(
+			'redirector'
+		);
+
+		$params = [
+			$request->getModuleName(),
+			$request->getControllerName(),
+			$request->getActionName(),
+		];
+
+		$id = $request->getParam('id');
+
+		if ($id !== null && $id !== '') {
+			$params[] = $id;
+		}
+
+		$redirector->gotoSimple(
+			'login',
+			'user',
+			'users',
+			[
+				'url' => implode('|', $params),
+			]
+		);
+	}
+
+	private function denyAccess(
+		Zend_Controller_Request_Abstract $request,
+		bool $canViewController = false,
+		?string $resolvedController = null
+	): void {
+		$this->logDenied($request);
+
+		$flashMessenger =
+			Zend_Controller_Action_HelperBroker::getStaticHelper(
+				'FlashMessenger'
+			);
+
+		$flashMessenger->addMessage(
+			'MESSAGES_ACCESS_DENIED'
+		);
+
+		$redirector =
+			Zend_Controller_Action_HelperBroker::getStaticHelper(
+				'redirector'
+			);
+
+		if ($canViewController && $resolvedController !== null) {
+			$redirector->gotoSimple(
+				'index',
+				$resolvedController,
+				$request->getModuleName()
+			);
+
+			return;
+		}
+
+		$redirector->gotoSimple(
+			'index',
+			'index',
+			'default'
+		);
+	}
+
+	private function logPermit(
+		Zend_Controller_Request_Abstract $request
+	): void {
+		error_log(
+			'PERMIT/'
+			. $request->getModuleName()
+			. '/'
+			. $request->getControllerName()
+			. '/'
+			. $request->getActionName()
+		);
+	}
+
+	private function logDenied(
+		Zend_Controller_Request_Abstract $request
+	): void {
+		error_log(
+			'NO_PERMIT/'
+			. $request->getModuleName()
+			. '/'
+			. $request->getControllerName()
+			. '/'
+			. $request->getActionName()
+		);
+
+		error_log(
+			'NO_PERMIT'
+			. $request->getRequestUri()
+		);
 	}
 }
---
+++ library/DEEC/Permission.php
@@ -0,0 +1,217 @@
+<?php
+
+class DEEC_Permission
+{
+	private const ACTIONS = [
+		'add' => [
+			'add',
+			'copy',
+			'generate',
+			'send',
+			'upload',
+		],
+		'edit' => [
+			'edit',
+			'import',
+			'lock',
+			'unlock',
+			'keepalive',
+			'pin',
+			'apply',
+			'sort',
+			'save',
+			'cancel',
+		],
+		'view' => [
+			'view',
+			'get',
+			'index',
+			'search',
+			'select',
+			'history',
+			'suggest',
+			'export',
+			'preview',
+			'download',
+		],
+		'delete' => [
+			'delete',
+		],
+	];
+
+	private array $user;
+
+	private ?array $permissions = null;
+
+	public function __construct(array $user)
+	{
+		$this->user = $user;
+	}
+
+	public function hasRoutePermission(
+		Zend_Controller_Request_Abstract $request
+	): bool {
+		$module = $request->getModuleName();
+		$controller = $this->resolveController($request);
+		$action = $this->resolveAction($request->getActionName());
+
+		return $this->hasActionPermission(
+			$module,
+			$controller,
+			$action
+		);
+	}
+
+	public function hasActionPermission(
+		string $module,
+		string $controller,
+		string $action
+	): bool {
+		$permission = $this->getPermissionForAction($action);
+
+		if ($permission === null) {
+			return false;
+		}
+
+		return $this->hasPermission(
+			$module,
+			$controller,
+			$permission
+		);
+	}
+
+	public function hasPermission(
+		string $module,
+		string $controller,
+		string $permission
+	): bool {
+		$controllerPermissions = $this->getControllerPermissions(
+			$module,
+			$controller
+		);
+
+		return in_array(
+			$permission,
+			$controllerPermissions,
+			true
+		);
+	}
+
+	public function hasModule(string $module): bool
+	{
+		return array_key_exists($module, $this->getPermissions());
+	}
+
+	public function hasController(
+		string $module,
+		string $controller
+	): bool {
+		$permissions = $this->getPermissions();
+
+		return isset($permissions[$module])
+			&& array_key_exists(
+				$controller,
+				$permissions[$module]
+			);
+	}
+
+	public function getControllerPermissions(
+		string $module,
+		string $controller
+	): array {
+		$permissions = $this->getPermissions();
+
+		if (!isset($permissions[$module][$controller])) {
+			return [];
+		}
+
+		if (!is_array($permissions[$module][$controller])) {
+			return [];
+		}
+
+		return array_values(
+			array_unique($permissions[$module][$controller])
+		);
+	}
+
+	public function getPermissionForAction(
+		string $action
+	): ?string {
+		$action = $this->resolveAction($action);
+
+		foreach (self::ACTIONS as $permission => $actions) {
+			if (in_array($action, $actions, true)) {
+				return $permission;
+			}
+		}
+
+		return null;
+	}
+
+	public function resolveController(
+		Zend_Controller_Request_Abstract $request
+	): string {
+		$module = $request->getModuleName();
+		$controller = $request->getControllerName();
+
+		if (substr($controller, -3) === 'pos') {
+			$controller = substr($controller, 0, -3);
+		}
+
+		if (
+			$controller === 'position'
+			|| $controller === 'positionset'
+		) {
+			$parent = (string)$request->getParam('parent', '');
+
+			if ($parent !== '') {
+				$controller = $parent;
+			}
+		}
+
+		if ($module === 'contacts') {
+			return 'contact';
+		}
+
+		if ($module === 'items') {
+			return 'item';
+		}
+
+		return $controller;
+	}
+
+	public function resolveAction(string $action): string
+	{
+		if (strpos($action, 'generate') !== false) {
+			return 'generate';
+		}
+
+		return $action;
+	}
+
+	private function getPermissions(): array
+	{
+		if ($this->permissions !== null) {
+			return $this->permissions;
+		}
+
+		$permissionsDb = new Application_Model_DbTable_Permission();
+		$row = $permissionsDb->getPermissions();
+
+		$this->permissions = [];
+
+		foreach ($row as $module => $value) {
+			if (!is_string($value) || $value === '') {
+				continue;
+			}
+
+			$decoded = json_decode($value, true);
+
+			if (is_array($decoded)) {
+				$this->permissions[$module] = $decoded;
+			}
+		}
+
+		return $this->permissions;
+	}
+}
