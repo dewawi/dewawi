4a81c9f900ab4720ebd2b5659264724b9a81a17d
kseones <remzi@dewawi.com>
2026-08-02 14:34:41 +0200

    add unique position toolbar search ids

--- application/views/helpers/ToolbarPositions.php
+++ application/views/helpers/ToolbarPositions.php
@@ -2,28 +2,53 @@
 
 class Zend_View_Helper_ToolbarPositions extends Zend_View_Helper_Abstract
 {
-	public function toolbarPositions($class = null)
-	{
-		$view = $this->view;
-		$toolbar = $view->toolbarPositions;
+	public function toolbarPositions(
+		string $location,
+		int $setId = 0
+	): string {
+		$toolbar = $this->view->toolbarPositions;
 
-		if (!$toolbar || !is_object($toolbar) || !method_exists($toolbar, 'renderElement')) {
+		if (
+			!is_object($toolbar)
+			|| !method_exists($toolbar, 'renderElement')
+		) {
 			return '';
 		}
 
-		$classes = 'dw-toolbar dw-toolbar--positions toolbar positions';
-		if (!empty($class)) {
-			$classes .= ' ' . $class;
-		}
+		$searchId = 'search-position-'
+			. $location
+			. '-'
+			. $setId;
 
-		$html = '<div class="' . $classes . '">';
+		$html = '<div'
+			. ' class="dw-toolbar dw-toolbar--positions"'
+			. ' data-location="' . $this->escape($location) . '"'
+			. '>';
+
 		$html .= $toolbar->renderElement('add-position');
-		$html .= $toolbar->renderElement('search-position');
+
+		$html .= $toolbar->renderElementWithAttribs(
+			'search-position',
+			[
+				'id' => $searchId,
+			]
+		);
+
 		$html .= $toolbar->renderElement('select-position');
 		$html .= $toolbar->renderElement('copy-selected-position');
 		$html .= $toolbar->renderElement('delete-selected-position');
+
 		$html .= '</div>';
 
 		return $html;
+	}
+
+	protected function escape(string $value): string
+	{
+		return htmlspecialchars(
+			$value,
+			ENT_QUOTES,
+			'UTF-8'
+		);
 	}
 }
