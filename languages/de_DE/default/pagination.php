f06d88e32ccc267ac2c05cf400890ca494dcfd81
kseones <remzi@dewawi.com>
2026-08-02 16:54:43 +0200

    add pagination translations

---
+++ languages/de_DE/default/pagination.php
@@ -0,0 +1,8 @@
+<?php return array(
+	//Pagination
+	'PAGINATION_DISPLAYED' => 'Angezeigt',
+	'PAGINATION_TOTAL' => 'Gesamt',
+	'PAGINATION_SHOW' => 'Anzeigen',
+	'PAGINATION_PAGE' => 'Seite',
+	'PAGINATION_OF' => 'von',
+);
