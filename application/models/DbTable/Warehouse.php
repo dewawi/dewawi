f4f8a3c51af05885fc61430dfb2e6317cbc16114
kseones <remzi@dewawi.com>
2026-08-16 09:27:57 +0200

    refactor warehouse model to use entity base

--- application/models/DbTable/Warehouse.php
+++ application/models/DbTable/Warehouse.php
@@ -1,44 +1,27 @@
 <?php
 
-class Application_Model_DbTable_Warehouse extends Zend_Db_Table_Abstract
+class Application_Model_DbTable_Warehouse extends DEEC_Model_DbTable_Entity
 {
-
 	protected $_name = 'warehouse';
-
-	protected $_date = null;
-
-	protected $_user = null;
-
-	protected $_client = null;
-
-	public function init()
-	{
-		$this->_date = date('Y-m-d H:i:s');
-		$this->_user = Zend_Registry::get('User');
-		$this->_client = Zend_Registry::get('Client');
-	}
-
-	public function getWarehouse($id)
-	{
-		$id = (int)$id;
-		$row = $this->fetchRow('id = ' . $id);
-		if (!$row) {
-			throw new Exception("Could not find row $id");
-		}
-		return $row->toArray();
-	}
 
 	public function getWarehouses()
 	{
-		$where = array();
+		$where = [];
 		$where[] = $this->getAdapter()->quoteInto('clientid = ?', $this->_client['id']);
+		$where[] = $this->getAdapter()->quoteInto('active = ?', 1);
 		$where[] = $this->getAdapter()->quoteInto('deleted = ?', 0);
-		$data = $this->fetchAll($where);
 
-		$uoms = array();
-		foreach($data as $uom) {
-			$uoms[$uom->id] = $uom->title;
+		return $this->fetchAll($where, ['default DESC', 'title ASC']);
+	}
+
+	public function getSelectOptions(): array
+	{
+		$warehouses = [];
+
+		foreach($this->getWarehouses() as $warehouse) {
+			$warehouses[(int)$warehouse->id] = $warehouse->title;
 		}
-		return $uoms;
+
+		return $warehouses;
 	}
 }
