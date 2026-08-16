bee1ec4ea45d12562952e8eb83ab8a6ce61e4737
kseones <remzi@dewawi.com>
2026-08-16 08:38:28 +0200

    add German stock translations

---
+++ languages/de_DE/items/stock.php
@@ -0,0 +1,8 @@
+<?php return array(
+	'ITEMS_STOCK' => 'Lagerbestand',
+	'ITEMS_STOCK_QUANTITY' => 'Bestand',
+	'ITEMS_STOCK_RESERVED' => 'Reserviert',
+	'ITEMS_STOCK_AVAILABLE' => 'Verfügbar',
+	'ITEMS_STOCK_INCOMING' => 'Im Zulauf',
+	'ITEMS_WAREHOUSE_CODE' => 'Lagercode',
+);
