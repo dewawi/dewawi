be7ecdd6790461126a8099f3c90bf6d791f1ff59
kseones <remzi@dewawi.com>
2026-08-20 08:09:28 +0200

    simplify variant editing

--- application/modules/items/views/helpers/Variants.php
+++ application/modules/items/views/helpers/Variants.php
@@ -9,8 +9,6 @@ class Zend_View_Helper_Variants extends Zend_View_Helper_Abstract
 			[
 				'item' => $this->view->item,
 				'variants' => $this->view->itemVariants,
-				'variantOptions' => $this->view->itemVariantOptions,
-				'availableOptions' => $this->view->itemVariantAvailableOptions,
 			]
 		);
 	}
