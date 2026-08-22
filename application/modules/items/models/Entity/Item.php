5235ec467edcc65b9e337c73c42b5a3029f1ca84
kseones <remzi@dewawi.com>
2026-08-22 14:46:26 +0200

    define item variant field responsibilities

--- application/modules/items/models/Entity/Item.php
+++ application/modules/items/models/Entity/Item.php
@@ -2,6 +2,81 @@
 
 class Items_Model_Entity_Item
 {
+	private const INHERITED_FIELDS = [
+		'title',
+		'type',
+		'catid',
+		'description',
+		'info',
+		'taxid',
+		'currency',
+		'uomid',
+		'manufacturerid',
+		'ctn',
+		'origincountry',
+		'originregion',
+		'deliverytime',
+		'deliverytimeoos',
+		'video',
+		'shopid',
+		'shopdescription',
+		'shopdescriptionshort',
+		'shopdescriptionmini',
+	];
+
+	private const VARIANT_FIELDS = [
+		'sku',
+		'gtin',
+		'manufacturersku',
+		'manufacturergtin',
+		'cost',
+		'price',
+		'specialprice',
+		'margin',
+		'quantity',
+		'inventory',
+		'minquantity',
+		'orderquantity',
+		'quantityreminder',
+		'warehouseid',
+		'width',
+		'length',
+		'height',
+		'weight',
+		'packwidth',
+		'packlength',
+		'packheight',
+		'packweight',
+	];
+
+	public static function inheritedFields(): array
+	{
+		return self::INHERITED_FIELDS;
+	}
+
+	public static function variantFields(): array
+	{
+		return self::VARIANT_FIELDS;
+	}
+
+	public static function isInheritedField(string $field): bool
+	{
+		return in_array(
+			$field,
+			self::INHERITED_FIELDS,
+			true
+		);
+	}
+
+	public static function isVariantField(string $field): bool
+	{
+		return in_array(
+			$field,
+			self::VARIANT_FIELDS,
+			true
+		);
+	}
+
 	public static function listConfig(): array
 	{
 		return [
