b92e4d4aa66be9c69cbacba5eeafd11ca0ce9f1c
kseones <remzi@dewawi.com>
2026-09-04 11:50:17 +0200

    add slug path resolution

--- application/modules/shops/models/DbTable/Slug.php
+++ application/modules/shops/models/DbTable/Slug.php
@@ -26,6 +26,60 @@ class Shops_Model_DbTable_Slug extends Zend_Db_Table_Abstract
 		return $data ? $data->toArray() : $data;
 	}
 
+	public function getEntitySlug($controller, $entityid)
+	{
+		$where = array();
+		$where[] = $this->getAdapter()->quoteInto('module = ?', 'shops');
+		$where[] = $this->getAdapter()->quoteInto('controller = ?', $controller);
+		$where[] = $this->getAdapter()->quoteInto('entityid = ?', (int)$entityid);
+		$where[] = $this->getAdapter()->quoteInto('shopid = ?', (int)$this->_shop['id']);
+		$where[] = $this->getAdapter()->quoteInto('clientid = ?', (int)$this->_shop['clientid']);
+		$where[] = $this->getAdapter()->quoteInto('deleted = ?', 0);
+
+		$data = $this->fetchRow($where);
+
+		return $data ? $data->toArray() : null;
+	}
+
+	public function getPath($controller, $entityid)
+	{
+		$key = $controller . ':' . (int)$entityid;
+
+		if (array_key_exists($key, $this->_pathCache)) {
+			return $this->_pathCache[$key];
+		}
+
+		$item = $this->getEntitySlug($controller, $entityid);
+
+		if (!$item || empty($item['slug'])) {
+			return $this->_pathCache[$key] = null;
+		}
+
+		$path = trim($item['slug'], '/');
+		$visited = array();
+
+		while (!empty($item['parentid'])) {
+			$parentController = $item['controller'] === 'item' ? 'category' : $item['controller'];
+			$parentKey = $parentController . ':' . (int)$item['parentid'];
+
+			if (isset($visited[$parentKey])) {
+				break;
+			}
+
+			$visited[$parentKey] = true;
+			$parent = $this->getEntitySlug($parentController, $item['parentid']);
+
+			if (!$parent || empty($parent['slug'])) {
+				break;
+			}
+
+			$item = $parent;
+			$path = trim($item['slug'], '/') . '/' . $path;
+		}
+
+		return $this->_pathCache[$key] = $path;
+	}
+
 	public function getSlugs($shopid)
 	{
 		$shopid = (int)$shopid;
