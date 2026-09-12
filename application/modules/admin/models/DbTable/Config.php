bb65c46aa4268894f1628af8afe50689c36b1557
kseones <remzi@dewawi.com>
2026-09-12 17:08:47 +0200

    fix config edit locking

--- application/modules/admin/models/DbTable/Config.php
+++ application/modules/admin/models/DbTable/Config.php
@@ -29,6 +29,32 @@ class Admin_Model_DbTable_Config extends DEEC_Model_DbTable_Entity
 		$this->update($data, $where);
 	}
 
+	public function lock(int $id): void
+	{
+		$where = [
+			$this->getAdapter()->quoteInto('id = ?', $id),
+			$this->getAdapter()->quoteInto('clientid = ?', $this->getClientId()),
+		];
+
+		$this->update([
+			'locked' => $this->getUserId(),
+			'lockedtime' => $this->_date,
+		], $where);
+	}
+
+	public function unlock(int $id): void
+	{
+		$where = [
+			$this->getAdapter()->quoteInto('id = ?', $id),
+			$this->getAdapter()->quoteInto('clientid = ?', $this->getClientId()),
+		];
+
+		$this->update([
+			'locked' => 0,
+			'lockedtime' => null,
+		], $where);
+	}
+
 	public function getConfig($id)
 	{
 		$id = (int)$id;
