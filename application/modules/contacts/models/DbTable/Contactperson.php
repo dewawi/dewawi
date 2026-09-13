b1251636911c51b18a96fe8eb6e0c2fa33eb9e55
kseones <remzi@dewawi.com>
2026-09-13 10:28:09 +0200

    delete contact person emails

--- application/modules/contacts/models/DbTable/Contactperson.php
+++ application/modules/contacts/models/DbTable/Contactperson.php
@@ -3,4 +3,16 @@
 class Contacts_Model_DbTable_Contactperson extends DEEC_Model_DbTable_Entity
 {
 	protected $_name = 'contactperson';
+
+	public function deleteById(int $id): void
+	{
+		$emailDb = new Contacts_Model_DbTable_Email();
+		$emails = $emailDb->getByParentId($id, 'contacts', 'contactperson');
+
+		foreach ($emails as $email) {
+			$emailDb->deleteById((int)$email['id']);
+		}
+
+		parent::deleteById($id);
+	}
 }
