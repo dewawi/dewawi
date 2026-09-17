75c3f0a99b7ec77fad5dbf0926df4e05b4a17e7b
kseones <remzi@dewawi.com>
2026-09-17 21:04:12 +0200

    add page block storage

--- install/config/structure.sql
+++ install/config/structure.sql
@@ -1606,6 +1606,30 @@ CREATE TABLE `page` (
   KEY (clientid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS `pageblock` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parentid` int(11) NOT NULL DEFAULT 0,
+  `pageid` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `data` text DEFAULT NULL,
+  `ordering` int(11) NOT NULL DEFAULT 0,
+  `clientid` int(11) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `createdby` int(11) NOT NULL DEFAULT 0,
+  `modified` datetime DEFAULT NULL,
+  `modifiedby` int(11) NOT NULL DEFAULT 0,
+  `locked` int(11) NOT NULL DEFAULT 0,
+  `lockedtime` datetime DEFAULT NULL,
+  `activated` tinyint(1) NOT NULL DEFAULT 0,
+  `deleted` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY (`parentid`),
+  KEY (`pageid`),
+  KEY (`type`),
+  KEY (`clientid`),
+  KEY (`deleted`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE IF NOT EXISTS `paymentmethod` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) DEFAULT NULL,
---
+++ install/config/update-1.1.6-1.1.7.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS `pageblock` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parentid` int(11) NOT NULL DEFAULT 0,
+  `pageid` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `data` text DEFAULT NULL,
+  `ordering` int(11) NOT NULL DEFAULT 0,
+  `clientid` int(11) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `createdby` int(11) NOT NULL DEFAULT 0,
+  `modified` datetime DEFAULT NULL,
+  `modifiedby` int(11) NOT NULL DEFAULT 0,
+  `locked` int(11) NOT NULL DEFAULT 0,
+  `lockedtime` datetime DEFAULT NULL,
+  `activated` tinyint(1) NOT NULL DEFAULT 0,
+  `deleted` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY (`parentid`),
+  KEY (`pageid`),
+  KEY (`type`),
+  KEY (`clientid`),
+  KEY (`deleted`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
