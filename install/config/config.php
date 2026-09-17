e3051c19d77e320e01b690c10ed1c318e57bc4e3
kseones <remzi@dewawi.com>
2026-09-17 20:48:16 +0200

    Release: 1.1.6

--- install/config/config.php
+++ install/config/config.php
@@ -6,7 +6,7 @@
 	// INTRODUCTION
 	$introduction = array();
 	$introduction["product"] = "DEWAWI";
-	$introduction["productVersion"] = "1.1.5";
+	$introduction["productVersion"] = "1.1.6";
 	$introduction["company"] = "DEWAWI Open Source";
 
 	// SERVER REQUIREMENTS
