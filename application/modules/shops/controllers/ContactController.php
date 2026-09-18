7c974571ee428015292156e13725eb47a172e2f5
kseones <remzi@dewawi.com>
2026-09-18 21:07:15 +0200

    migrate contact controller to site base

--- application/modules/shops/controllers/ContactController.php
+++ application/modules/shops/controllers/ContactController.php
@@ -1,405 +1,56 @@
 <?php
 
-class Shops_ContactController extends Zend_Controller_Action
+class Shops_ContactController extends Shops_Controller_Action
 {
-	protected $_date = null;
-
-	protected $_user = null;
-
 	protected $contactDataSession;
-
-	/**
-	 * FlashMessenger
-	 *
-	 * @var Zend_Controller_Action_Helper_FlashMessenger
-	 */
-	protected $_flashMessenger = null;
 
 	public function init()
 	{
-		$params = $this->_getAllParams();
-
-		$this->_date = date('Y-m-d H:i:s');
-
-		$this->view->id = isset($params['id']) ? $params['id'] : 0;
-		$this->view->action = $params['action'];
-		$this->view->controller = $params['controller'];
-		$this->view->module = $params['module'];
-
-		$this->_flashMessenger = $this->_helper->getHelper('FlashMessenger');
-
-		//Check if the directory is writable
-		//if($this->view->id) $this->view->dirwritable = $this->_helper->Directory->isWritable($this->view->id, 'item', $this->_flashMessenger);
-		//if($this->view->id) $this->view->dirwritable = $this->_helper->Directory->isWritable($this->view->id, 'media', $this->_flashMessenger);
-
-		$this->cart = new Shops_Model_ShoppingCart();
-
-		// Make the cart accessible in all views
-		$this->view->cart = $this->cart;
+		parent::init();
 
 		$this->contactDataSession = new Zend_Session_Namespace('ShopsContact');
-	}
-
-	public function indexAction()
-	{
-		$shop = Zend_Registry::get('Shop');
-
-		$this->_helper->getHelper('layout')->setLayout('site');
-
-		$toolbar = new Items_Form_Toolbar();
-		//$options = $this->_helper->Options->getOptions($toolbar);
-		$params = $this->_helper->Params->getParams($toolbar);
-
-		$contact = new Shops_Form_Contact();
-		$this->view->contact = $contact;
-
-		// Falls Werte vorhanden sind, ins Formular laden
-		if (!empty($this->contactDataSession->formData)) {
-			$contact->populate($this->contactDataSession->formData);
-		}
-
-		$categoryDb = new Shops_Model_DbTable_Category();
-		$categories = $categoryDb->getCategories();
-
-		$images = array();
-		$imageDb = new Shops_Model_DbTable_Media();
-		$images['categories'] = $imageDb->getCategoryMedia($categories);
-
-		$menuDb = new Shops_Model_DbTable_Menu();
-		$menus = $menuDb->getMenus($shop['id']);
-
-		$menuitems = array();
-		$menuitemDb = new Shops_Model_DbTable_Menuitem();
-		foreach($menus as $menu) {
-			$menuitems[$menu->id] = $menuitemDb->getMenuitems($menu->id);
-		}
-
-		//$this->view->tags = $tags;
-		//$this->view->tagEntites = $tagEntites;
-		$this->view->shop = $shop;
-		$this->view->images = $images;
-		$this->view->menus = $menus;
-		$this->view->menuitems = $menuitems;
-		$this->view->categories = $categories;
-		//$this->view->pagination = $this->_helper->Pagination->getPagination($toolbar, $params, $records, count($items));
-		$this->view->messages = $this->_flashMessenger->getMessages();
-	}
-
-	public function searchAction()
-	{
-		$type = $this->_getParam('type', 'index');
-
-		$this->_helper->viewRenderer->setRender($type);
-		$this->_helper->getHelper('layout')->disableLayout();
-
-		$form = new Shops_Form_Account();
-		$toolbar = new Shops_Form_Toolbar();
-		$options = $this->_helper->Options->getOptions($toolbar);
-		$params = $this->_helper->Params->getParams($toolbar, $options);
-
-		$get = new Shops_Model_Get();
-		$stats = array();
-		$items = array();
-		$accounts = $get->accounts($params, $options);
-		foreach($accounts as $account) {
-			$params['limit'] = 0;
-			$params['shopid'] = $account['id'];
-			list($items[$account['id']], $records) = $get->items($params, $options);
-			$stats[$account['id']]['total'] = count($items[$account['id']]);
-			$stats[$account['id']]['listed'] = 0;
-			foreach($items[$account['id']] as $item) {
-				if($item->listedby) ++$stats[$account['id']]['listed'];
-			}
-		}
-
-		$this->view->form = $form;
-		$this->view->stats = $stats;
-		$this->view->accounts = $accounts;
-		$this->view->options = $options;
-		$this->view->toolbar = $toolbar;
-		$this->view->messages = $this->_flashMessenger->getMessages();
 	}
 
 	public function sendAction()
 	{
 		$request = $this->getRequest();
-		$data = $request->getPost();
 
-		// Nur POST zulassen
 		if (!$request->isPost()) {
 			return $this->_helper->redirector->gotoSimple('index', 'index', 'shops');
 		}
 
-		// Save to session
+		$data = $request->getPost();
 		$this->contactDataSession->formData = $data;
 
-		// 1) Honeypot: wenn ausgefüllt → Bot, tue so als wäre alles ok, aber sende nichts
 		if (!empty($data['fax_number'])) {
-			// Bot erkannt – stillschweigend auf Erfolg leiten
 			return $this->_helper->redirector->gotoRoute([], 'contact_success', true);
 		}
 
-		// Send email
 		$this->_helper->Email->sendEmail('shops', 'contact', 'contact');
 
-		$this->_helper->redirector->gotoRoute([], 'contact_success', true);
+		return $this->_helper->redirector->gotoRoute([], 'contact_success', true);
 	}
 
 	public function successAction()
 	{
-		/*if(empty($this->contactDataSession->formData)) {
-			return $this->_helper->redirector->gotoSimple('index', 'index', 'default');
-		}*/
+		$this->initSiteLayout();
 
-		$shop = Zend_Registry::get('Shop');
+		$this->view->formData = $this->contactDataSession->formData ?? [];
 
-		// Holt die Formulardaten aus der Session
-		$this->view->formData = $this->contactDataSession->formData;
-
-		$this->_helper->getHelper('layout')->setLayout('site');
-
-		$toolbar = new Items_Form_Toolbar();
-		//$options = $this->_helper->Options->getOptions($toolbar);
-		$params = $this->_helper->Params->getParams($toolbar);
-
-		$contact = new Shops_Form_Contact();
-		$this->view->contact = $contact;
-
-		$categoryDb = new Shops_Model_DbTable_Category();
-		$categories = $categoryDb->getCategories();
-
-		$images = array();
-		$imageDb = new Shops_Model_DbTable_Media();
-		$images['categories'] = $imageDb->getCategoryMedia($categories);
-
-		$menuDb = new Shops_Model_DbTable_Menu();
-		$menus = $menuDb->getMenus($shop['id']);
-
-		$menuitems = array();
-		$menuitemDb = new Shops_Model_DbTable_Menuitem();
-		foreach($menus as $menu) {
-			$menuitems[$menu->id] = $menuitemDb->getMenuitems($menu->id);
-		}
-
-		//$this->view->tags = $tags;
-		//$this->view->tagEntites = $tagEntites;
-		$this->view->shop = $shop;
-		$this->view->menus = $menus;
-		$this->view->images = $images;
-		$this->view->menus = $menus;
-		$this->view->menuitems = $menuitems;
-		$this->view->categories = $categories;
-		//$this->view->pagination = $this->_helper->Pagination->getPagination($toolbar, $params, $records, count($items));
-		$this->view->messages = $this->_flashMessenger->getMessages();
+		$this->assignMessages();
 	}
 
 	public function errorAction()
 	{
-		$shop = Zend_Registry::get('Shop');
+		$this->initSiteLayout();
 
-		$this->_helper->getHelper('layout')->setLayout('site');
+		$this->view->formData = [
+			'name' => $this->_getParam('name'),
+			'email' => $this->_getParam('email'),
+			'subject' => $this->_getParam('subject'),
+			'message' => $this->_getParam('message'),
+		];
 
-		$toolbar = new Items_Form_Toolbar();
-		//$options = $this->_helper->Options->getOptions($toolbar);
-		$params = $this->_helper->Params->getParams($toolbar);
-
-		$contact = new Shops_Form_Contact();
-		$this->view->contact = $contact;
-
-		$categoryDb = new Shops_Model_DbTable_Category();
-		$categories = $categoryDb->getCategories();
-
-		$images = array();
-		$imageDb = new Shops_Model_DbTable_Media();
-		$images['categories'] = $imageDb->getCategoryMedia($categories);
-
-		$menuDb = new Shops_Model_DbTable_Menu();
-		$menus = $menuDb->getMenus($shop['id']);
-
-		$menuitems = array();
-		$menuitemDb = new Shops_Model_DbTable_Menuitem();
-		foreach($menus as $menu) {
-			$menuitems[$menu->id] = $menuitemDb->getMenuitems($menu->id);
-		}
-
-		// Retrieve the form data from the forwarded request
-		$name = $this->_getParam('name');
-		$email = $this->_getParam('email');
-		$subject = $this->_getParam('subject');
-		$message = $this->_getParam('message');
-
-		// Pass the data to the view
-		$this->view->formData = compact('name', 'email', 'subject', 'message');
-
-		//$this->view->tags = $tags;
-		//$this->view->tagEntites = $tagEntites;
-		$this->view->shop = $shop;
-		$this->view->menus = $menus;
-		$this->view->images = $images;
-		$this->view->menus = $menus;
-		$this->view->menuitems = $menuitems;
-		$this->view->categories = $categories;
-		//$this->view->pagination = $this->_helper->Pagination->getPagination($toolbar, $params, $records, count($items));
-		$this->view->messages = $this->_flashMessenger->getMessages();
-	}
-
-	public function syncAction()
-	{
-		$this->_helper->viewRenderer->setNoRender();
-		$this->_helper->getHelper('layout')->disableLayout();
-
-		$shopid = $this->_getParam('shopid', 0);
-
-		if($shopid) {
-			$accountDb = new Shops_Model_DbTable_Account();
-			$account = $accountDb->getAccount($shopid);
-
-			if($account) {
-				$config = parse_ini_file(BASE_PATH.'/configs/database.ini');
-
-				// DB Settings 
-				define('DB_SERVER', $config['resources.db.params.host']);
-				define('DB_USER', $config['resources.db.params.username']);
-				define('DB_PASSWORD', $config['resources.db.params.password']);
-				define('DB_NAME', $config['resources.db.params.dbname']);
-
-				require_once(BASE_PATH.'/library/DEEC/Shop.php');
-				$Shops = new DEEC_Shop(BASE_PATH, DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
-				$Shops->listItems($shopid);
-
-				$accountDb->updateAccount($shopid, array('updated' => date('Y-m-d H:i:s'), 'updatedby' => $this->_user['id']));
-
-				$this->_flashMessenger->addMessage('MESSAGES_RECORDS_SUCCESFULLY_UPDATED');
-			}
-		}
-
-		$this->_helper->redirector->gotoSimple('index', 'index');
-	}
-
-	public function addAction()
-	{
-		header('Content-type: application/json');
-		$this->_helper->viewRenderer->setNoRender();
-		$this->_helper->getHelper('layout')->disableLayout();
-
-		$request = $this->getRequest();
-		if($request->isPost()) {
-			$form = new Shops_Form_Account();
-			$options = $this->_helper->Options->getOptions($form);
-			$params = $this->_helper->Params->getParams($form, $options);
-			$data = $request->getPost();
-			if($form->isValid($data)) {
-				$accountDb = new Shops_Model_DbTable_Account();
-				$id = $accountDb->addAccount($data);
-				echo Zend_Json::encode($accountDb->getAccount($id));
-			} else {
-				echo Zend_Json::encode(array('message' => $this->view->translate('MESSAGES_FORM_IS_INVALID')));
-			}
-		}
-	}
-
-	public function editAction()
-	{
-		$request = $this->getRequest();
-		$id = $this->_getParam('id', 0);
-		$activeTab = $request->getCookie('tab', null);
-
-		$accountDb = new Shops_Model_DbTable_Account();
-		$account = $accountDb->getAccount($id);
-
-		if(false) {
-			$this->_helper->redirector->gotoSimple('view', 'account', null, array('id' => $id));
-		} else {
-			$this->_helper->Access->lock($id, $this->_user['id'], $account['locked'], $account['lockedtime']);
-
-			$form = new Shops_Form_Account();
-			$options = $this->_helper->Options->getOptions($form);
-
-			if($request->isPost()) {
-				$this->_helper->viewRenderer->setNoRender();
-				$this->_helper->getHelper('layout')->disableLayout();
-				$data = $request->getPost();
-				$element = key($data);
-				if(isset($form->$element) && $form->isValidPartial($data)) {
-					$accountDb->updateAccount($id, $data);
-					echo Zend_Json::encode($accountDb->getAccount($id));
-				} else {
-					echo Zend_Json::encode(array('message' => $this->view->translate('MESSAGES_FORM_IS_INVALID')));
-				}
-			} else {
-				if($id > 0) {
-					$form->populate($account);
-
-					//Toolbar
-					$toolbar = new Shops_Form_Toolbar();
-
-					$this->view->form = $form;
-					$this->view->activeTab = $activeTab;
-					$this->view->toolbar = $toolbar;
-				}
-			}
-		}
-		$this->view->messages = array_merge(
-			$this->_helper->flashMessenger->getMessages(),
-			$this->_helper->flashMessenger->getCurrentMessages()
-		);
-		$this->_helper->flashMessenger->clearCurrentMessages();
-	}
-
-	public function copyAction()
-	{
-		$this->_helper->viewRenderer->setNoRender();
-		$this->_helper->getHelper('layout')->disableLayout();
-
-		$id = $this->_getParam('id', 0);
-		$item = new Shops_Model_DbTable_Item();
-		$data = $item->getItem($id);
-		unset($data['id']);
-		$data['quantity'] = 0;
-		$data['inventory'] = 1;
-		$data['title'] = $data['title'].' 2';
-		$data['modified'] = NULL;
-		$data['modifiedby'] = 0;
-		$data['locked'] = 0;
-		$data['lockedtime'] = NULL;
-		echo $itemid = $item->addItem($data);
-
-		$this->_flashMessenger->addMessage('MESSAGES_SUCCESFULLY_COPIED');
-	}
-
-	public function deleteAction()
-	{
-		$this->_helper->viewRenderer->setNoRender();
-		$this->_helper->getHelper('layout')->disableLayout();
-
-		if ($this->getRequest()->isPost()) {
-			$id = $this->_getParam('id', 0);
-			$item = new Shops_Model_DbTable_Item();
-			$item->deleteItem($id);
-		}
-		$this->_flashMessenger->addMessage('MESSAGES_SUCCESFULLY_DELETED');
-	}
-
-	public function lockAction()
-	{
-		$id = $this->_getParam('id', 0);
-		$this->_helper->Access->lock($id, $this->_user['id']);
-	}
-
-	public function unlockAction()
-	{
-		$id = $this->_getParam('id', 0);
-		$this->_helper->Access->unlock($id);
-	}
-
-	public function keepaliveAction()
-	{
-		$id = $this->_getParam('id', 0);
-		$this->_helper->Access->keepalive($id);
-	}
-
-	public function validateAction()
-	{
-		$this->_helper->Validate();
+		$this->assignMessages();
 	}
 }
