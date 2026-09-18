1a0fd8de6dc58a8f7db0cca8277c9112c7b889ca
kseones <remzi@dewawi.com>
2026-09-18 19:55:00 +0200

    fix campaign email composer mode

--- application/views/helpers/EmailMessages.php
+++ application/views/helpers/EmailMessages.php
@@ -1,18 +1,21 @@
 <?php
 
-public function EmailMessages(array $options = [])
+class Zend_View_Helper_EmailMessages extends Zend_View_Helper_Abstract
 {
-	$options += [
-		'mode' => 'send',
-	];
+	public function EmailMessages(array $options = [])
+	{
+		$options += [
+			'mode' => 'send',
+		];
 
-	return $this->view->partial('partials/email-messages.phtml', [
-		'emailForm' => $this->view->emailForm,
-		'attachments' => $this->view->attachments,
-		'documentUrl' => $this->view->documentUrl,
-		'module' => $this->view->module,
-		'controller' => $this->view->controller,
-		'id' => $this->view->id,
-		'mode' => $options['mode'],
-	]);
+		return $this->view->partial('partials/email-messages.phtml', [
+			'emailForm' => $this->view->emailForm,
+			'attachments' => $this->view->attachments,
+			'documentUrl' => $this->view->documentUrl,
+			'module' => $this->view->module,
+			'controller' => $this->view->controller,
+			'id' => $this->view->id,
+			'mode' => $options['mode'],
+		]);
+	}
 }
