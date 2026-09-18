8a73f7327f76f6c5a84b0cc47bb3c61c1764b6da
kseones <remzi@dewawi.com>
2026-09-18 20:32:39 +0200

    remove legacy page controller actions

--- application/modules/shops/controllers/PageController.php
+++ application/modules/shops/controllers/PageController.php
@@ -10,13 +10,9 @@ class Shops_PageController extends Shops_Controller_Action
 		$id = (int)$this->_getParam('id', 0);
 
 		$toolbar = new Items_Form_Toolbar();
-		$params = $this->_helper->Params->getParams($toolbar);
 
 		$contact = new Shops_Form_Contact();
 		$this->view->contact = $contact;
-
-		$get = new Shops_Model_Get();
-		$tags = $get->tags('shops', 'category');
 
 		$pageDb = new Shops_Model_DbTable_Page();
 		$page = $pageDb->getPage($id, (int)$shop['id']);
@@ -31,7 +27,6 @@ class Shops_PageController extends Shops_Controller_Action
 
 		$imageDb = new Shops_Model_DbTable_Media();
 
-		$this->view->tags = $tags;
 		$this->view->page = $page;
 		$this->view->pageblocks = $pageblocks;
 		$this->view->images = [
@@ -40,200 +35,5 @@ class Shops_PageController extends Shops_Controller_Action
 		$this->view->toolbar = $toolbar;
 
 		$this->assignMessages();
-	}
-
-	public function searchAction()
-	{
-		$type = $this->_getParam('type', 'index');
-
-		$this->_helper->viewRenderer->setRender($type);
-		$this->_helper->getHelper('layout')->disableLayout();
-
-		$form = new Shops_Form_Account();
-		$toolbar = new Shops_Form_Toolbar();
-		$options = $this->_helper->Options->getOptions($toolbar);
-		$params = $this->_helper->Params->getParams($toolbar, $options);
-
-		$get = new Shops_Model_Get();
-		$stats = array();
-		$items = array();
-		$accounts = $get->accounts($params, $options);
-		foreach($accounts as $account) {
-			$params['limit'] = 0;
-			$params['shopid'] = $account['id'];
-			list($items[$account['id']], $records) = $get->items($params, $options);
-			$stats[$account['id']]['total'] = count($items[$account['id']]);
-			$stats[$account['id']]['listed'] = 0;
-			foreach($items[$account['id']] as $item) {
-				if($item->listedby) ++$stats[$account['id']]['listed'];
-			}
-		}
-
-		$this->view->form = $form;
-		$this->view->stats = $stats;
-		$this->view->accounts = $accounts;
-		$this->view->options = $options;
-		$this->view->toolbar = $toolbar;
-		$this->view->messages = $this->_flashMessenger->getMessages();
-	}
-
-	public function syncAction()
-	{
-		$this->_helper->viewRenderer->setNoRender();
-		$this->_helper->getHelper('layout')->disableLayout();
-
-		$shopid = $this->_getParam('shopid', 0);
-
-		if($shopid) {
-			$accountDb = new Shops_Model_DbTable_Account();
-			$account = $accountDb->getAccount($shopid);
-
-			if($account) {
-				$config = parse_ini_file(BASE_PATH.'/configs/database.ini');
-
-				// DB Settings 
-				define('DB_SERVER', $config['resources.db.params.host']);
-				define('DB_USER', $config['resources.db.params.username']);
-				define('DB_PASSWORD', $config['resources.db.params.password']);
-				define('DB_NAME', $config['resources.db.params.dbname']);
-
-				require_once(BASE_PATH.'/library/DEEC/Shop.php');
-				$Shops = new DEEC_Shop(BASE_PATH, DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
-				$Shops->listItems($shopid);
-
-				$accountDb->updateAccount($shopid, array('updated' => date('Y-m-d H:i:s'), 'updatedby' => $this->_user['id']));
-
-				$this->_flashMessenger->addMessage('MESSAGES_RECORDS_SUCCESFULLY_UPDATED');
-			}
-		}
-
-		$this->_helper->redirector->gotoSimple('index', 'index');
-	}
-
-	public function addAction()
-	{
-		header('Content-type: application/json');
-		$this->_helper->viewRenderer->setNoRender();
-		$this->_helper->getHelper('layout')->disableLayout();
-
-		$request = $this->getRequest();
-		if($request->isPost()) {
-			$form = new Shops_Form_Account();
-			$options = $this->_helper->Options->getOptions($form);
-			$params = $this->_helper->Params->getParams($form, $options);
-			$data = $request->getPost();
-			if($form->isValid($data)) {
-				$accountDb = new Shops_Model_DbTable_Account();
-				$id = $accountDb->addAccount($data);
-				echo Zend_Json::encode($accountDb->getAccount($id));
-			} else {
-				echo Zend_Json::encode(array('message' => $this->view->translate('MESSAGES_FORM_IS_INVALID')));
-			}
-		}
-	}
-
-	public function editAction()
-	{
-		$request = $this->getRequest();
-		$id = $this->_getParam('id', 0);
-		$activeTab = $request->getCookie('tab', null);
-
-		$accountDb = new Shops_Model_DbTable_Account();
-		$account = $accountDb->getAccount($id);
-
-		if(false) {
-			$this->_helper->redirector->gotoSimple('view', 'account', null, array('id' => $id));
-		} else {
-			$this->_helper->Access->lock($id, $this->_user['id'], $account['locked'], $account['lockedtime']);
-
-			$form = new Shops_Form_Account();
-			$options = $this->_helper->Options->getOptions($form);
-
-			if($request->isPost()) {
-				$this->_helper->viewRenderer->setNoRender();
-				$this->_helper->getHelper('layout')->disableLayout();
-				$data = $request->getPost();
-				$element = key($data);
-				if(isset($form->$element) && $form->isValidPartial($data)) {
-					$accountDb->updateAccount($id, $data);
-					echo Zend_Json::encode($accountDb->getAccount($id));
-				} else {
-					echo Zend_Json::encode(array('message' => $this->view->translate('MESSAGES_FORM_IS_INVALID')));
-				}
-			} else {
-				if($id > 0) {
-					$form->populate($account);
-
-					//Toolbar
-					$toolbar = new Shops_Form_Toolbar();
-
-					$this->view->form = $form;
-					$this->view->activeTab = $activeTab;
-					$this->view->toolbar = $toolbar;
-				}
-			}
-		}
-		$this->view->messages = array_merge(
-			$this->_helper->flashMessenger->getMessages(),
-			$this->_helper->flashMessenger->getCurrentMessages()
-		);
-		$this->_helper->flashMessenger->clearCurrentMessages();
-	}
-
-	public function copyAction()
-	{
-		$this->_helper->viewRenderer->setNoRender();
-		$this->_helper->getHelper('layout')->disableLayout();
-
-		$id = $this->_getParam('id', 0);
-		$item = new Shops_Model_DbTable_Item();
-		$data = $item->getItem($id);
-		unset($data['id']);
-		$data['quantity'] = 0;
-		$data['inventory'] = 1;
-		$data['title'] = $data['title'].' 2';
-		$data['modified'] = NULL;
-		$data['modifiedby'] = 0;
-		$data['locked'] = 0;
-		$data['lockedtime'] = NULL;
-		echo $itemid = $item->addItem($data);
-
-		$this->_flashMessenger->addMessage('MESSAGES_SUCCESFULLY_COPIED');
-	}
-
-	public function deleteAction()
-	{
-		$this->_helper->viewRenderer->setNoRender();
-		$this->_helper->getHelper('layout')->disableLayout();
-
-		if ($this->getRequest()->isPost()) {
-			$id = $this->_getParam('id', 0);
-			$item = new Shops_Model_DbTable_Item();
-			$item->deleteItem($id);
-		}
-		$this->_flashMessenger->addMessage('MESSAGES_SUCCESFULLY_DELETED');
-	}
-
-	public function lockAction()
-	{
-		$id = $this->_getParam('id', 0);
-		$this->_helper->Access->lock($id, $this->_user['id']);
-	}
-
-	public function unlockAction()
-	{
-		$id = $this->_getParam('id', 0);
-		$this->_helper->Access->unlock($id);
-	}
-
-	public function keepaliveAction()
-	{
-		$id = $this->_getParam('id', 0);
-		$this->_helper->Access->keepalive($id);
-	}
-
-	public function validateAction()
-	{
-		$this->_helper->Validate();
 	}
 }
