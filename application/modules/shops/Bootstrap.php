9f376dbe84245f83bc28e4a0cf02576a9978ef91
kseones <remzi@dewawi.com>
2026-09-18 20:14:07 +0200

    add site controller base

--- application/modules/shops/Bootstrap.php
+++ application/modules/shops/Bootstrap.php
@@ -2,6 +2,10 @@
 
 class Shops_Bootstrap extends Zend_Application_Module_Bootstrap
 {
+	protected function _initControllerAutoload() {
+		$this->getResourceLoader()->addResourceType('controllerbase', 'controllers', 'Controller');
+	}
+
 	protected function _initPlugins() {
 		$front = Zend_Controller_Front::getInstance();
 		$front->registerPlugin(new Shops_Plugin_Helper());
