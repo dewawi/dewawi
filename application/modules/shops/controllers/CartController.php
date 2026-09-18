0d0ccb52035bdb81a5ff22b1f695ba2f533f0094
kseones <remzi@dewawi.com>
2026-09-18 21:16:21 +0200

    migrate cart controller to site base

--- application/modules/shops/controllers/CartController.php
+++ application/modules/shops/controllers/CartController.php
@@ -1,86 +1,34 @@
 <?php
-class Shops_CartController extends Zend_Controller_Action
-{
-	private $cart;
 
+class Shops_CartController extends Shops_Controller_Action
+{
 	public function init()
 	{
-		$params = $this->_getAllParams();
-
-		$this->_date = date('Y-m-d H:i:s');
-
-		$this->view->id = isset($params['id']) ? $params['id'] : 0;
-		$this->view->action = $params['action'];
-		$this->view->controller = $params['controller'];
-		$this->view->module = $params['module'];
-
-		$this->_flashMessenger = $this->_helper->getHelper('FlashMessenger');
-
-		//Check if the directory is writable
-		//if($this->view->id) $this->view->dirwritable = $this->_helper->Directory->isWritable($this->view->id, 'item', $this->_flashMessenger);
-		//if($this->view->id) $this->view->dirwritable = $this->_helper->Directory->isWritable($this->view->id, 'media', $this->_flashMessenger);
-
-		$this->cart = new Shops_Model_ShoppingCart();
-
-		// Make the cart accessible in all views
-		$this->view->cart = $this->cart;
+		parent::init();
+		$this->initSiteCart();
 	}
 
 	public function indexAction()
 	{
-		$shop = Zend_Registry::get('Shop');
+		$this->initSiteLayout();
 
-		$this->_helper->getHelper('layout')->setLayout('site');
-
-		$toolbar = new Items_Form_Toolbar();
-		//$options = $this->_helper->Options->getOptions($toolbar);
-		$params = $this->_helper->Params->getParams($toolbar);
-		//print_r($params);
-		//print_r($this->getRequest()->getParams());
-		$contact = new Shops_Form_Contact();
-		$this->view->contact = $contact;
-
-		$categoryDb = new Shops_Model_DbTable_Category();
-		$categories = $categoryDb->getCategories();
-
-		$menuDb = new Shops_Model_DbTable_Menu();
-		$menus = $menuDb->getMenus($shop['id']);
-
-		$menuitems = array();
-		$menuitemDb = new Shops_Model_DbTable_Menuitem();
-		foreach($menus as $menu) {
-			$menuitems[$menu->id] = $menuitemDb->getMenuitems($menu->id);
-		}
-
-		$images = array();
-		$imageDb = new Shops_Model_DbTable_Media();
-		$images['categories'] = $imageDb->getCategoryMedia($categories);
-
-		//$this->view->tags = $tags;
-		//$this->view->tagEntites = $tagEntites;
-		$this->view->shop = $shop;
-		$this->view->images = $images;
-		$this->view->menus = $menus;
-		$this->view->menuitems = $menuitems;
-		$this->view->categories = $categories;
-		//$this->view->pagination = $this->_helper->Pagination->getPagination($toolbar, $params, $records, count($items));
-		$this->view->messages = $this->_flashMessenger->getMessages();
-
-		//Get currency
 		$currency = $this->_helper->Currency->getCurrency('EUR', 'USE_SYMBOL');
 
-		// Retrieve cart items for display
-		$items = array();
-		foreach($this->cart->getItems() as $id => $item) {
-			$items[$id]['title'] = $item['title'];
-			$items[$id]['sku'] = $item['sku'];
-			$items[$id]['quantity'] = $item['quantity'];
-			$items[$id]['total'] = $currency->toCurrency($item['price']*$item['quantity']);
-			$items[$id]['price'] = $currency->toCurrency($item['price']);
+		$items = [];
+		foreach ($this->cart->getItems() as $id => $item) {
+			$items[$id] = [
+				'title' => $item['title'],
+				'sku' => $item['sku'],
+				'quantity' => $item['quantity'],
+				'total' => $currency->toCurrency($item['price'] * $item['quantity']),
+				'price' => $currency->toCurrency($item['price']),
+			];
 		}
 
 		$this->view->items = $items;
 		$this->view->total = $currency->toCurrency($this->cart->getTotalPrice());
+
+		$this->assignMessages();
 	}
 
 	public function addAction()
@@ -89,7 +37,7 @@ class Shops_CartController extends Zend_Controller_Action
 			return $this->_helper->json(['success' => false, 'message' => 'Invalid request']);
 		}
 
-		$shop = Zend_Registry::get('Shop');
+		$shop = $this->_site;
 		$id = (int)$this->_getParam('id');
 		$quantity = $this->_getParam('quantity', 1);
 
@@ -118,6 +66,10 @@ class Shops_CartController extends Zend_Controller_Action
 
 	public function updateAction()
 	{
+		if (!$this->getRequest()->isPost()) {
+			return $this->_helper->json(['success' => false, 'message' => 'Invalid request']);
+		}
+
 		$id = (int)$this->_getParam('id');
 		$quantity = $this->_getParam('quantity');
 
@@ -149,6 +101,10 @@ class Shops_CartController extends Zend_Controller_Action
 
 	public function removeAction()
 	{
+		if (!$this->getRequest()->isPost()) {
+			return $this->_helper->json(['success' => false, 'message' => 'Invalid request']);
+		}
+
 		$id = (int)$this->_getParam('id');
 
 		if(!$id || !$this->cart->removeItem($id)) {
@@ -165,6 +121,10 @@ class Shops_CartController extends Zend_Controller_Action
 
 	public function clearAction()
 	{
+		if (!$this->getRequest()->isPost()) {
+			return $this->_helper->json(['success' => false, 'message' => 'Invalid request']);
+		}
+
 		$this->cart->clearCart();
 
 		return $this->_helper->json([
