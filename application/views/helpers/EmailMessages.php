91521f181f35fab6dcb2eebbfcb2f76766b4f516
kseones <remzi@dewawi.com>
2026-09-18 19:46:23 +0200

    add campaign email composer mode

--- application/views/helpers/EmailMessages.php
+++ application/views/helpers/EmailMessages.php
@@ -1,18 +1,18 @@
 <?php
-/**
-* Class inserts neccery code for Messages	
-*/
-class Zend_View_Helper_EmailMessages extends Zend_View_Helper_Abstract
+
+public function EmailMessages(array $options = [])
 {
-	public function EmailMessages()
-	{
-		return $this->view->partial('partials/email-messages.phtml', [
-			'emailForm' => $this->view->emailForm,
-			'attachments' => $this->view->attachments,
-			'documentUrl' => $this->view->documentUrl,
-			'module' => $this->view->module,
-			'controller' => $this->view->controller,
-			'id' => $this->view->id,
-		]);
-	}
+	$options += [
+		'mode' => 'send',
+	];
+
+	return $this->view->partial('partials/email-messages.phtml', [
+		'emailForm' => $this->view->emailForm,
+		'attachments' => $this->view->attachments,
+		'documentUrl' => $this->view->documentUrl,
+		'module' => $this->view->module,
+		'controller' => $this->view->controller,
+		'id' => $this->view->id,
+		'mode' => $options['mode'],
+	]);
 }
