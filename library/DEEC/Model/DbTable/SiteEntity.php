44d6910863116c75c7445d9895cdaea83400d535
kseones <remzi@dewawi.com>
2026-09-19 10:44:28 +0200

    add site entity public scope

--- library/DEEC/Model/DbTable/SiteEntity.php
+++ library/DEEC/Model/DbTable/SiteEntity.php
@@ -4,6 +4,7 @@ abstract class DEEC_Model_DbTable_SiteEntity extends DEEC_Model_DbTable_Entity
 {
 	protected ?DEEC_Site_Context $_siteContext = null;
 	protected ?string $siteField = 'shopid';
+	protected ?string $publicField = null;
 
 	public function init()
 	{
@@ -20,6 +21,43 @@ abstract class DEEC_Model_DbTable_SiteEntity extends DEEC_Model_DbTable_Entity
 	public function getSiteId(): int
 	{
 		return $this->_siteContext->getSiteId();
+	}
+
+	protected function getPublicSelect(): Zend_Db_Table_Select
+	{
+		$select = $this->select();
+
+		foreach ($this->getAccessWhere() as $where) {
+			$select->where($where);
+		}
+
+		if ($this->deletedField !== null) {
+			$select->where($this->deletedField . ' = ?', 0);
+		}
+
+		if ($this->publicField !== null) {
+			$select->where($this->publicField . ' = ?', 1);
+		}
+
+		return $select;
+	}
+
+	public function getPublicById(int $id): ?array
+	{
+		$row = $this->fetchRow(
+			$this->getPublicSelect()
+				->where('id = ?', $id)
+				->limit(1)
+		);
+
+		return $row ? $row->toArray() : null;
+	}
+
+	protected function applyPublicScope(Zend_Db_Table_Select $select): void
+	{
+		if (in_array('activated', $this->info(self::COLS), true)) {
+			$select->where('activated = ?', 1);
+		}
 	}
 
 	protected function getAccessWhere(): array
