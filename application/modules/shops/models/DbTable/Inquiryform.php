96f5d20edcba86280e56653d4faa86319b656210
kseones <remzi@dewawi.com>
2026-09-19 13:43:27 +0200

    migrate site inquiry forms to entity base

--- application/modules/shops/models/DbTable/Inquiryform.php
+++ application/modules/shops/models/DbTable/Inquiryform.php
@@ -1,30 +1,11 @@
 <?php
 
-class Shops_Model_DbTable_Inquiryform extends Zend_Db_Table_Abstract
+class Shops_Model_DbTable_Inquiryform extends DEEC_Model_DbTable_SiteEntity
 {
-
 	protected $_name = 'shopinquiryform';
 
-	protected $_date = null;
-
-	protected $_user = null;
-
-	protected $_shop = null;
-
-	public function init()
+	public function getInquiryform(int $id): ?array
 	{
-		$this->_date = date('Y-m-d H:i:s');
-		$this->_shop = Zend_Registry::get('Shop');
-	}
-
-	public function getInquiryform($id)
-	{
-		$where = [
-			$this->getAdapter()->quoteInto('id = ?', $id),
-			$this->getAdapter()->quoteInto('clientid = ?', $this->_shop['clientid']),
-			$this->getAdapter()->quoteInto('deleted = ?', 0),
-		];
-		$row = $this->fetchRow($where);
-		return $row ? $row->toArray() : null;
+		return $this->getById($id);
 	}
 }
