905bd2dd9216b962fd0451c408b4aafeba0e17c4
kseones <remzi@dewawi.com>
2026-09-19 08:50:48 +0200

    migrate sitemap controller to site base

--- application/modules/shops/controllers/SitemapController.php
+++ application/modules/shops/controllers/SitemapController.php
@@ -1,132 +1,56 @@
 <?php
 
-class Shops_SitemapController extends Zend_Controller_Action
+class Shops_SitemapController extends Shops_Controller_Action
 {
-	protected $_date = null;
-
-	protected $_user = null;
-
-	/**
-	 * FlashMessenger
-	 *
-	 * @var Zend_Controller_Action_Helper_FlashMessenger
-	 */
-	protected $_flashMessenger = null;
-
-	public function init()
-	{
-		$params = $this->_getAllParams();
-
-		$this->_date = date('Y-m-d H:i:s');
-
-		$this->view->id = isset($params['id']) ? $params['id'] : 0;
-		$this->view->action = $params['action'];
-		$this->view->controller = $params['controller'];
-		$this->view->module = $params['module'];
-
-		$this->_flashMessenger = $this->_helper->getHelper('FlashMessenger');
-
-		//Check if the directory is writable
-		//if($this->view->id) $this->view->dirwritable = $this->_helper->Directory->isWritable($this->view->id, 'item', $this->_flashMessenger);
-		//if($this->view->id) $this->view->dirwritable = $this->_helper->Directory->isWritable($this->view->id, 'media', $this->_flashMessenger);
-
-		$this->cart = new Shops_Model_ShoppingCart();
-
-		// Make the cart accessible in all views
-		$this->view->cart = $this->cart;
-	}
-
 	public function indexAction()
 	{
-		// Disable the view renderer (we're outputting XML directly)
-		$this->_helper->viewRenderer->setNoRender(true);
+		$this->_helper->viewRenderer->setNoRender();
 		$this->_helper->layout->disableLayout();
 
-		// Set the content type to XML
-		$this->getResponse()->setHeader('Content-Type', 'application/xml');
+		$this->getResponse()->setHeader('Content-Type', 'application/xml; charset=UTF-8', true);
 
-		// Initialize the base URL
-		$baseUrl = $this->getRequest()->getScheme() . '://' . $this->getRequest()->getHttpHost();
+		$baseUrl = rtrim($this->getRequest()->getScheme() . '://' . $this->getRequest()->getHttpHost(), '/');
+		$slugDb = new Shops_Model_DbTable_Slug();
+		$slugs = $slugDb->getSlugs((int)$this->_site['id']);
 
-		// Begin XML output
-		echo '<?xml version="1.0" encoding="UTF-8"?>';
-		echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+		$xml = '<?xml version="1.0" encoding="UTF-8"?>';
+		$xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+		$xml .= $this->renderUrl($baseUrl . '/', 'daily', '1.0');
 
-		// Add the homepage
-		echo '<url>';
-		echo '<loc>' . $baseUrl . '/</loc>';
-		echo '<changefreq>daily</changefreq>';
-		echo '<priority>1.0</priority>';
-		echo '</url>';
+		$urls = [$baseUrl . '/' => true];
 
-		// Get shop details from the registry
-		$shop = Zend_Registry::get('Shop');
-
-		// Add shop's home URL
-		echo '<url>';
-		echo '<loc>' . $shop['url'] . '</loc>';
-		echo '<changefreq>weekly</changefreq>';
-		echo '<priority>0.8</priority>';
-		echo '</url>';
-
-		// Fetch slugs for the current shop
-		$slugTable = new Zend_Db_Table('slug');
-		$slugs = $slugTable->fetchAll(['shopid = ?' => $shop['id']]);
-
-		// Organize slugs into a dictionary with entityid as the key
-		$slugDict = [];
 		foreach ($slugs as $slug) {
-			$slugDict[$slug['entityid']] = $slug;
+			if ($slug['module'] !== 'shops' || empty($slug['slug']) || empty($slug['controller']) || empty($slug['entityid'])) {
+				continue;
+			}
+
+			$path = $slugDb->getPath($slug['controller'], (int)$slug['entityid']);
+
+			if (!$path) {
+				continue;
+			}
+
+			$url = $baseUrl . '/' . ltrim($path, '/');
+
+			if (isset($urls[$url])) {
+				continue;
+			}
+
+			$urls[$url] = true;
+			$xml .= $this->renderUrl($url, 'weekly', '0.6');
 		}
 
-		// Helper function to build the full slug path using entityid with parent-child hierarchy
-		$getFullSlug = function ($item, $slugDict) {
-			$slug = $item['slug'];
-			
-			// Continue while the item has a parent
-			while ($item['parentid']) {
-				// Find the parent item in the slugDict
-				if (isset($slugDict[$item['parentid']])) {
-					$parentItem = $slugDict[$item['parentid']];
-					// Prepend the parent's slug to the current slug
-					$slug = $parentItem['slug'] . '/' . $slug;
-					$item = $parentItem;
-				} else {
-					break; // Parent not found, stop the loop
-				}
-			}
-			
-			return $slug;
-		};
+		$xml .= '</urlset>';
 
-		// Loop through categories and add to the sitemap
-		foreach ($slugs as $slug) {
-			if (!empty($slug['slug'])) { // Ensure slug exists
-				$fullSlug = $getFullSlug($slug, $slugDict); // Get full slug path
-				$slugUrl = $shop['url'] . '/' . $fullSlug;
-				echo '<url>';
-				echo '<loc>' . htmlspecialchars($slugUrl) . '</loc>';
-				echo '<changefreq>weekly</changefreq>';
-				echo '<priority>0.6</priority>';
-				echo '</url>';
-			}
-		}
+		$this->getResponse()->setBody($xml);
+	}
 
-		// Get items for this shop
-		//$itemTable = new Zend_Db_Table('item');
-		//$items = $itemTable->fetchAll(['shopid = ?' => $shop['id']]);
-
-		// Loop through items and add to the sitemap
-		/*foreach ($items as $item) {
-			$itemUrl = $shop['url'] . '/' . $category['slug'] . '/' . $item['slug'];
-			echo '<url>';
-			echo '<loc>' . htmlspecialchars($itemUrl) . '</loc>';
-			echo '<changefreq>monthly</changefreq>';
-			echo '<priority>0.5</priority>';
-			echo '</url>';
-		}*/
-
-		// Close the XML tags
-		echo '</urlset>';
+	protected function renderUrl(string $url, string $changefreq, string $priority): string
+	{
+		return '<url>'
+			. '<loc>' . htmlspecialchars($url, ENT_QUOTES | ENT_XML1, 'UTF-8') . '</loc>'
+			. '<changefreq>' . $changefreq . '</changefreq>'
+			. '<priority>' . $priority . '</priority>'
+			. '</url>';
 	}
 }
