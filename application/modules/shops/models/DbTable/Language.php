44fa06a9e71f4d15ba9ab4ea4d84f674f6bae71d
kseones <remzi@dewawi.com>
2026-09-19 13:46:43 +0200

    migrate site languages to entity base

--- application/modules/shops/models/DbTable/Language.php
+++ application/modules/shops/models/DbTable/Language.php
@@ -1,45 +1,22 @@
 <?php
 
-class Shops_Model_DbTable_Language extends Zend_Db_Table_Abstract
+class Shops_Model_DbTable_Language extends DEEC_Model_DbTable_SiteEntity
 {
-
 	protected $_name = 'language';
+	protected ?string $siteField = null;
 
-	protected $_date = null;
-
-	protected $_user = null;
-
-	protected $_shop = null;
-
-	public function init()
+	public function getPrimaryLanguage(): array
 	{
-		$this->_date = date('Y-m-d H:i:s');
-		$this->_shop = Zend_Registry::get('Shop');
-	}
+		$row = $this->fetchRow(
+			$this->getPublicSelect()
+				->order('ordering ASC')
+				->limit(1)
+		);
 
-	public function getPrimaryLanguage()
-	{
-		$where = array();
-		$where[] = $this->getAdapter()->quoteInto('clientid = ?', $this->_shop['clientid']);
-		$where[] = $this->getAdapter()->quoteInto('deleted = ?', 0);
-		$data = $this->fetchRow($where, 'ordering');
-		if(!$data) {
-			throw new Exception("Could not find language");
+		if (!$row) {
+			throw new RuntimeException('Could not find language');
 		}
-		return $data->toArray();
-	}
 
-	public function getLanguages()
-	{
-		$where = array();
-		$where[] = $this->getAdapter()->quoteInto('clientid = ?', $this->_shop['clientid']);
-		$where[] = $this->getAdapter()->quoteInto('deleted = ?', 0);
-		$data = $this->fetchAll($where);
-
-		$languages = array();
-		foreach($data as $language) {
-			$languages[$language->code] = $language->name;
-		}
-		return $languages;
+		return $row->toArray();
 	}
 }
