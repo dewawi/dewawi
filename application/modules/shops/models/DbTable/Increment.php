a9899c9d9b8e59694d535be2a84463d96f570379
kseones <remzi@dewawi.com>
2026-09-19 14:06:19 +0200

    migrate site increments to entity base

--- application/modules/shops/models/DbTable/Increment.php
+++ application/modules/shops/models/DbTable/Increment.php
@@ -1,45 +1,27 @@
 <?php
 
-class Shops_Model_DbTable_Increment extends Zend_Db_Table_Abstract
+class Shops_Model_DbTable_Increment extends DEEC_Model_DbTable_SiteEntity
 {
-
 	protected $_name = 'increment';
+	protected ?string $siteField = null;
+	protected ?string $deletedField = null;
+	protected ?string $orderingField = null;
 
-	protected $_date = null;
-
-	protected $_user = null;
-
-	protected $_shop = null;
-
-	public function init()
+	public function getIncrement(string $type): int
 	{
-		$this->_date = date('Y-m-d H:i:s');
-		$this->_shop = Zend_Registry::get('Shop');
-	}
+		$row = $this->fetchRow($this->getPublicSelect()->limit(1));
 
-	public function getIncrement($type)
-	{
-		$where = array();
-		$where[] = $this->getAdapter()->quoteInto('clientid = ?', $this->_shop['clientid']);
-		$data = $this->fetchRow($where);
-		if(!$data) {
-			throw new Exception("Could not find row $type");
+		if (!$row) {
+			throw new RuntimeException("Could not find increment $type");
 		}
-		return $data->$type;
+
+		return (int)$row->$type;
 	}
 
-	public function setIncrement($increment, $type)
+	public function setIncrement(int $increment, string $type): void
 	{
-		// Increment by 3
-		// TODO move increment value to the db
-		$increment += 1;
-
-		$data = array(
-			$type => $increment,
-		);
-
-		// Update only the specific column for the client
-		$where = $this->getAdapter()->quoteInto('clientid = ?', (int)$this->_shop['clientid']);
-		$this->update($data, $where);
+		$this->update([
+			$type => $increment + 1,
+		], $this->getAccessWhere());
 	}
 }
