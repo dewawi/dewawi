59ee6f20acf5174ce612317590f68cc1494b09d2
kseones <remzi@dewawi.com>
2026-09-19 15:33:02 +0200

    render site menus

---
+++ application/modules/shops/views/helpers/RenderMenuItems.php
@@ -0,0 +1,33 @@
+<?php
+
+class Zend_View_Helper_RenderMenuItems extends Zend_View_Helper_Abstract
+{
+	public function RenderMenuItems($items, int $parentId = 0): string
+	{
+		$html = '';
+
+		foreach ($items as $item) {
+			if ((int)$item->parentid !== $parentId) {
+				continue;
+			}
+
+			$children = $this->RenderMenuItems($items, (int)$item->id);
+			$url = (int)$item->pageid > 0 ? $this->view->SlugUrl('page', (int)$item->pageid) : '';
+
+			if ($url === '' && $children === '') {
+				continue;
+			}
+
+			$html .= '<li class="nav-item">';
+			$html .= '<a class="nav-link" href="' . $this->view->escape($url !== '' ? $url : '#') . '">' . $this->view->escape($item->title) . '</a>';
+
+			if ($children !== '') {
+				$html .= '<ul class="submenu">' . $children . '</ul>';
+			}
+
+			$html .= '</li>';
+		}
+
+		return $html;
+	}
+}
