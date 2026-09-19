51305982ed12ae4cef53ddfc2ece7ad88fea7b2c
kseones <remzi@dewawi.com>
2026-09-19 13:57:13 +0200

    migrate site email templates to entity base

--- application/modules/shops/models/DbTable/Emailtemplate.php
+++ application/modules/shops/models/DbTable/Emailtemplate.php
@@ -1,29 +1,19 @@
 <?php
 
-class Shops_Model_DbTable_Emailtemplate extends Zend_Db_Table_Abstract
+class Shops_Model_DbTable_Emailtemplate extends DEEC_Model_DbTable_SiteEntity
 {
-
 	protected $_name = 'emailtemplate';
+	protected ?string $siteField = null;
+	protected ?string $deletedField = null;
 
-	protected $_date = null;
-
-	protected $_user = null;
-
-	protected $_shop = null;
-
-	public function init()
+	public function getEmailtemplate(string $module, string $controller): ?array
 	{
-		$this->_date = date('Y-m-d H:i:s');
-		$this->_shop = Zend_Registry::get('Shop');
-	}
+		$row = $this->fetchRow(
+			$this->getPublicSelect()
+				->where('module = ?', $module)
+				->where('controller = ?', $controller)
+		);
 
-	public function getEmailtemplate($module, $controller)
-	{
-		$where = array();
-		$where[] = $this->getAdapter()->quoteInto('module = ?', $module);
-		$where[] = $this->getAdapter()->quoteInto('controller = ?', $controller);
-		$where[] = $this->getAdapter()->quoteInto('clientid = ?', $this->_shop['clientid']);
-		$data = $this->fetchRow($where);
-		return $data ? $data->toArray() : $data;
+		return $row ? $row->toArray() : null;
 	}
 }
