c3cdef81a86c2a2a983230379bed3e8adf991bb9
kseones <remzi@dewawi.com>
2026-09-19 13:59:49 +0200

    migrate site contacts to entity base

--- application/modules/shops/models/DbTable/Contact.php
+++ application/modules/shops/models/DbTable/Contact.php
@@ -1,102 +1,29 @@
 <?php
 
-class Shops_Model_DbTable_Contact extends Zend_Db_Table_Abstract
+class Shops_Model_DbTable_Contact extends DEEC_Model_DbTable_SiteEntity
 {
-
 	protected $_name = 'contact';
+	protected ?string $siteField = null;
+	protected ?string $orderingField = null;
 
-	protected $_date = null;
-
-	protected $_user = null;
-
-	protected $_shop = null;
-
-	public function init()
+	public function getContact(int $id): ?array
 	{
-		$this->_date = date('Y-m-d H:i:s');
-		$this->_shop = Zend_Registry::get('Shop');
+		return $this->getById($id);
 	}
 
-	public function getContact($id)
+	public function getContactWithID(int $contactId): ?array
 	{
-		$id = (int)$id;
-		$row = $this->fetchRow('id = ' . $id);
-		if(!$row) return false;
-		return $row->toArray();
+		$row = $this->fetchRow(
+			$this->getPublicSelect()
+				->where('contactid = ?', $contactId)
+				->limit(1)
+		);
+
+		return $row ? $row->toArray() : null;
 	}
 
-	public function getContactWithID($contactid)
+	public function addContact(array $data): int
 	{
-		$contactid = (int)$contactid;
-		$where = array();
-		$where[] = $this->getAdapter()->quoteInto('contactid = ?', $contactid);
-		$where[] = $this->getAdapter()->quoteInto('clientid = ?', $this->_shop['clientid']);
-		$where[] = $this->getAdapter()->quoteInto('deleted = ?', 0);
-		$row = $this->fetchRow($where);
-		if(!$row) return false;
-		return $row->toArray();
-	}
-
-	public function getContactsByCategory($catid)
-	{
-		$catid = (int)$catid;
-		$row = $this->fetchAll('catid = ' . $catid);
-		if (!$row) {
-			throw new Exception("Could not find row $catid");
-		}
-		return $row->toArray();
-	}
-
-	public function getLatestContacts()
-	{
-		$where = array();
-		$where[] = $this->getAdapter()->quoteInto('clientid = ?', $this->_shop['clientid']);
-		$where[] = $this->getAdapter()->quoteInto('deleted = ?', 0);
-		$data = $this->fetchAll($where, 'id DESC', 5);
-		return $data;
-	}
-
-	public function addContact($data)
-	{
-		$data['clientid'] = $this->_shop['clientid'];
-		$data['created'] = $this->_date;
-		//$data['createdby'] = $this->_user['id'];
-		$this->insert($data);
-		return $this->getAdapter()->lastInsertId();
-	}
-
-	public function updateContact($id, $data)
-	{
-		$id = (int)$id;
-		$data['modified'] = $this->_date;
-		//$data['modifiedby'] = $this->_user['id'];
-		$where = $this->getAdapter()->quoteInto('id = ?', $id);
-		$this->update($data, $where);
-	}
-
-	public function lock($id)
-	{
-		$id = (int)$id;
-		$data = array();
-		//$data['locked'] = $this->_user['id'];
-		$data['lockedtime'] = $this->_date;
-		$where = $this->getAdapter()->quoteInto('id = ?', $id);
-		$this->update($data, $where);
-	}
-
-	public function unlock($id)
-	{
-		$id = (int)$id;
-		$data = array('locked' => 0);
-		$where = $this->getAdapter()->quoteInto('id = ?', $id);
-		$this->update($data, $where);
-	}
-
-	public function deleteContact($id)
-	{
-		$id = (int)$id;
-		$data = array('deleted' => 1);
-		$where = $this->getAdapter()->quoteInto('id = ?', $id);
-		$this->update($data, $where);
+		return $this->create($data);
 	}
 }
