825aece77bee624dfdd8cea200656406b2d663a3
kseones <remzi@dewawi.com>
2026-09-20 13:57:51 +0200

    initialize site cms structure

--- application/modules/admin/controllers/ShopController.php
+++ application/modules/admin/controllers/ShopController.php
@@ -18,6 +18,41 @@ class Admin_ShopController extends DEEC_Controller_AdminAction
 		];
 	}
 
+	protected function afterCreate(int $id, array $data): void
+	{
+		$pageDb = new Admin_Model_DbTable_Page();
+		$pageDb->create([
+			'shopid' => $id,
+			'parentid' => 0,
+			'type' => 'home',
+			'title' => $this->view->translate('ADMIN_HOME_PAGE'),
+			'ordering' => $pageDb->getNextOrdering([
+				'shopid' => $id,
+				'parentid' => 0,
+				'type' => 'home',
+			]),
+			'activated' => 1,
+		]);
+
+		$menuDb = new Admin_Model_DbTable_Menu();
+
+		foreach ([
+			'header' => 'ADMIN_HEADER_MENU',
+			'footer' => 'ADMIN_FOOTER_MENU',
+		] as $position => $title) {
+			$menuDb->create([
+				'shopid' => $id,
+				'title' => $this->view->translate($title),
+				'position' => $position,
+				'ordering' => $menuDb->getNextOrdering([
+					'shopid' => $id,
+					'position' => $position,
+				]),
+				'activated' => 1,
+			]);
+		}
+	}
+
 	protected function afterCopy(int $oldId, int $newId, array $oldRow, array $newRow): void
 	{
 		$configDb = new Admin_Model_DbTable_Config();
